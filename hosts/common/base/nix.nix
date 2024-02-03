dec0c1752c843341a0c171a9acf401177a633c0f
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-03 17:08:55 +0100

    removed error

--- hosts/common/base/nix.nix
+++ hosts/common/base/nix.nix
@@ -4,7 +4,7 @@
     # TODO
     # https://github.com/NixOS/nix/issues/9579
     # https://github.com/NixOS/nix/pull/9547
-    package = pkgs.nixVersions.nix_2_18;
+    # package = pkgs.nixVersions.nix_2_18;
 
     settings = {
       # substituters = [
