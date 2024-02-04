31ae346da43d1fb74dd172b04c6daf2d0d1aed33
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-04 11:52:15 +0100

    start of nvim config

--- home/eragon/features/nvim/default.nix
+++ home/eragon/features/nvim/default.nix
@@ -1,2 +1,13 @@
 {
+  pkgs,
+  lib,
+  ...
+}: {
+  home.packages = with pkgs; [
+    neovim
+  ];
+  # xdg.configFile."nvim".source
+  # builtins.fetchGit {
+  # url = "https://github.com/jarneamerlinck/kickstart.nvim";
+  # };
 }
