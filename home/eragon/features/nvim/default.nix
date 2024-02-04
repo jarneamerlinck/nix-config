56a4d071931289247d4f3a77814f49f9919f308a
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-04 11:57:23 +0100

    Cloning cofnig

--- home/eragon/features/nvim/default.nix
+++ home/eragon/features/nvim/default.nix
@@ -7,7 +7,7 @@
     neovim
   ];
   # xdg.configFile."nvim".source
-  # builtins.fetchGit {
-  # url = "https://github.com/jarneamerlinck/kickstart.nvim";
-  # };
+  builtins.fetchGit {
+    url = "https://github.com/jarneamerlinck/kickstart.nvim";
+  };
 }
