0dad03dd6d1afcb9d9134ce145f5672be0ec23fa
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-04 17:26:12 +0100

    refactor

--- home/eragon/features/nvim/default.nix
+++ home/eragon/features/nvim/default.nix
@@ -3,39 +3,40 @@
   lib,
   ...
 }: {
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-  };
   programs.zsh = {
     initExtraBeforeCompInit = ''
     eval "$(zoxide init zsh)"
     '';
-
-  home.shellAliases = {
-    v="nvim";
-    z="zoxide";
   };
+  home = {
+    shellAliases = {
+      v="nvim";
+    };
 
-  home.packages = with pkgs; [
-    neovim
-    ripgrep
-    xclip
-    lua
-    luarocks
-    go
-    shellcheck
-    nodejs
-    gcc
-    clangStdenv
-    python39
-    fzf
-    zoxide
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+    packages = with pkgs; [
+      neovim
+      ripgrep
+      xclip
+      lua
+      luarocks
+      go
+      shellcheck
+      nodejs
+      gcc
+      clangStdenv
+      python39
+      fzf
+      zoxide
 
-    vimPlugins.telescope-fzf-native-nvim
-    lazygit
+      vimPlugins.telescope-fzf-native-nvim
+      lazygit
 
-  ];
+    ];
+  };
   programs.neovim.extraConfig = ''
       mkdir -p ~/.config
       git clone https://github.com/jarneamerlinck/kickstart.nvim ~/.config/nvim
