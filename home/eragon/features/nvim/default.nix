2cc594fc8b2262ea6f429d2586a7f85be2b2b99b
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-04 12:29:31 +0100

    try with home.file

--- home/eragon/features/nvim/default.nix
+++ home/eragon/features/nvim/default.nix
@@ -6,7 +6,11 @@
   home.packages = with pkgs; [
     neovim
   ];
-  xdg.configFile."nvim".source =  builtins.fetchGit {
-    url = "https://github.com/jarneamerlinck/kickstart.nvim";
+  # xdg.configFile."nvim".source =  builtins.fetchGit {
+  #   url = "https://github.com/jarneamerlinck/kickstart.nvim";
+  # };
+  home.file.".config/nvim" = {
+    source = "git+https://github.com/jarneamerlinck/kickstart.nvim";
+    target = ".config/nvim";
   };
 }
