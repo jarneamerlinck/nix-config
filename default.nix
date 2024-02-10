d5d98a7eca88ac613b1b885650958d8aca74da53
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-10 17:25:19 +0100

    Added shell for deployment

---
+++ default.nix
@@ -0,0 +1,19 @@
+#default.nix
+with import <nixpkgs> {}; {
+  pullapiEnv = stdenv.mkDerivation {
+    name = "update";
+    buildInputs = [
+      neovim
+      git
+    ];
+    shellHook = ''
+      cd ~
+      rm -rf ./nix-config
+      git clone --recurse-submodules https://github.com/jarneamerlinck/nix-config
+      cd nix-config
+      git checkout dev
+      git submodule update --init --recursive
+      ./deploy.sh $HOST
+    '';
+  };
+}
