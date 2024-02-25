f748becb7499a376917f7adbf8a95d8073e3d4ca
jarneamerlinck <jarneamerlinck@pm.me>
2024-02-25 11:15:51 +0100

    Added gnome desktop

---
+++ home/eragon/features/desktop/common/default.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    # ./deluge.nix
+    ./discord.nix
+    # ./dragon.nix
+    ./firefox.nix
+    # ./font.nix
+    # ./gtk.nix
+    # ./kdeconnect.nix
+    # ./pavucontrol.nix
+
+  ];
+  xdg.portal.enable = true;
+}
--- home/eragon/vm1.nix
+++ home/eragon/vm1.nix
@@ -2,5 +2,6 @@
 {
   imports = [
     ./common
+    ./features/desktop/gnome
   ];
 }
