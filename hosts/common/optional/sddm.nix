3dba2646ee7c467515f600b76d2ca6460f05457f
jarneamerlinck <jarneamerlinck@pm.me>
2024-03-03 18:20:48 +0100

    Added sddm themes
    try to fix
    
    Fix attempt 2
    
    add file ext
    
    moved around
    
    add s
    
    moved around theme
    
    moving again
    
    typo fix
    
    added new theme to test with
    
    added new package
    
    moved to old theme
    
    try again
    
    try 2
    
    try another patht to theme
    
    added dependencies
    
    more dependencies
    
    Updates to themes
    
    path changes
    
    fix naming
    
    naming changes
    
    test second theme
    
    changed versions
    
    renamed for consistency
    
    typo fix
    
    typo refix

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -28,6 +28,15 @@
       enable = true;
       autoNumlock = true;
       settings.General.DisplayServer = "x11-user";
+      theme = "sddm-tokyo-night";
     };
   };
+  environment.systemPackages = with pkgs; [
+    sddm-themes.sddm-tokyo-night
+    libsForQt5.qt5.qtsvg
+    libsForQt5.qt5ct
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtquickcontrols
+    # libsForQt5.qt5.qtquickcontrols2
+  ];
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,6 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
-  # example = pkgs.callPackage ./example { };
-  sddm-sugar-dark = pkgs.callPackage ./sddm-sugar-dark { };
+{ pkgs ? import <nixpkgs> { } }: rec {
+  sddm-themes = pkgs.callPackage ./sddm-themes.nix { };
+  tokyo-night-sddm = pkgs.callPackage ./tokyo-night-sddm.nix { };
 }
