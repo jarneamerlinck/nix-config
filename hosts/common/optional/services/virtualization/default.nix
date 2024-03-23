187eeccdaa6e162068d02af1305058b2d42e2796
jarneamerlinck <jarneamerlinck@pm.me>
2024-03-23 23:04:25 +0100

    update for pre 23.11

--- hosts/common/optional/services/virtualization/default.nix
+++ hosts/common/optional/services/virtualization/default.nix
@@ -7,9 +7,12 @@
     distrobox
 
     quickemu
+    virt-manager
   ];
   virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
+  programs.dconf.enable = true;
+  # programs.virt-manager.enable = true; # for 23.11 or after
+
   virtualisation.docker = {
     enable = true;
   };
