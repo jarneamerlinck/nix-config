c446173e8b4bf536ca086e6635367738e417c1e4
jarneamerlinck <jarneamerlinck@pm.me>
2024-03-23 18:16:44 +0100

    Added Qemu

--- hosts/common/optional/services/virtualization/default.nix
+++ hosts/common/optional/services/virtualization/default.nix
@@ -3,9 +3,22 @@
 {
   environment.systemPackages = with pkgs; [
     flatpak
+    qemu
+    distrobox
+
+    quickemu
   ];
 
   virtualisation.docker = {
     enable = true;
   };
+
+  environment = {
+    (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+      qemu-system-x86_64 \
+      -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+      "$@"
+    '')
+  };
+
 }
