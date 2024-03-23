350d02ee7d171a9f27eb59dbcfa1f91c00ffe0d3
jarneamerlinck <jarneamerlinck@pm.me>
2024-03-23 18:18:45 +0100

    Removed code with bugs

--- hosts/common/optional/services/virtualization/default.nix
+++ hosts/common/optional/services/virtualization/default.nix
@@ -13,12 +13,4 @@
     enable = true;
   };
 
-  environment = {
-    (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
-      qemu-system-x86_64 \
-      -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-      "$@"
-    '')
-  };
-
 }
