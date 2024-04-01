965dc544beb59b178181376c9def803cfe2bbf41
jarneamerlinck <jarneamerlinck@pm.me>
2024-04-01 11:19:29 +0200

    Add host ash

---
+++ hosts/ash/default.nix
@@ -0,0 +1,66 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-gpu-amd
+    # inputs.hardware.nixosModules.common-pc-ssd
+
+    ./hardware-configuration.nix
+
+    ../common/base
+    ../common/users/eragon
+
+    # ../common/optional/auto-rebuild.nix
+    ../common/optional/unattended-upgrades.nix
+
+    ../common/optional/pipewire.nix
+    ../common/optional/services/virtualization
+
+  ];
+
+  nixpkgs = { # should go to global
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+  };
+  # Also to common
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  networking = {
+    hostName = "ash";
+    useDHCP = true;
+  };
+
+  system.stateVersion = "23.11";
+}
