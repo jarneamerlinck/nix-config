af98ddaa6514f5d55e3b8136a29c39e31b584aeb
jarneamerlinck <jarneamerlinck@pm.me>
2024-04-07 14:57:31 +0200

    removed activation of hyprland
    remove it again
    
    More removed
    
    Added modules

--- home/eragon/features/desktop/hyprland/default.nix
+++ home/eragon/features/desktop/hyprland/default.nix
@@ -1,13 +1,12 @@
 { lib, config, pkgs, inputs,  ... }: {
   imports = [
     ../common
+  ];
+  modules = [
     inputs.hyprland.homeManagerModules.default
   ];
   home.packages = with pkgs; [
     hyprland
     wayland
   ];
-
-
-  wayland.windowManager.hyprland.enable = true;
 }
