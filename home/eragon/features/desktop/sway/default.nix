01019bea0db828c59445f2aef2623d08bc1e94c4
jarneamerlinck <jarneamerlinck@pm.me>
2024-06-08 18:00:00 +0200

    Moved sway to home manager

--- home/eragon/features/desktop/sway/default.nix
+++ home/eragon/features/desktop/sway/default.nix
@@ -1,3 +1,15 @@
+{ inputs, lib, pkgs, config, outputs, ... }:
 {
   imports = [ ../common ];
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4"; # Super key
+      output = {
+        "Virtual-1" = {
+          mode = "1920x1080@60Hz";
+        };
+      };
+    };
+  };
 }
--- hosts/common/optional/sway.nix
+++ hosts/common/optional/sway.nix
@@ -1,12 +1,14 @@
 { pkgs, lib, config, ... }:
 {
-  programs.sway = {
-    enable = true;
-  };
+  # programs.sway = {
+  #   enable = true;
+  # };
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
   sound.enable = true;
+  security.polkit.enable = true;
+  # hardware.opengl.enable = true; # Only enable inside VM
   # nixpkgs.config.pulseaudio = true;
   # hardware.pulseaudio.enable = true;
 }
