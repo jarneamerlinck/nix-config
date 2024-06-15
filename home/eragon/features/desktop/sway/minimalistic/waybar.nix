e56eac59a42fe1b6fc11dad862a632158f819585
jarneamerlinck <jarneamerlinck@pm.me>
2024-06-15 14:32:06 +0200

    Layout for waybar

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -14,10 +14,6 @@
           layer = "top";
           position = "top";
           height = 30;
-          output = [
-            "eDP-1"
-            "HDMI-A-1"
-          ];
           modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
           modules-center = [ "sway/window" "custom/hello-from-waybar" ];
           modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
