999ef7ef20e122dce743d0f70417aab38ebe8fa9
jarneamerlinck <jarneamerlinck@pm.me>
2024-06-15 14:24:09 +0200

    Configs for waybar

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -1,8 +1,40 @@
 { pkgs, ... }: {
   home = {
     packages = with pkgs; [
-      waybar
+      # waybar
       wofi
     ];
   };
+  programs.waybar = {
+      enable = true;
+      systemd.enable = true;
+      style = builtins.readFile ./waybar_style.css;
+      settings = [{
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 30;
+          output = [
+            "eDP-1"
+            "HDMI-A-1"
+          ];
+          modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+          modules-center = [ "sway/window" "custom/hello-from-waybar" ];
+          modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+
+          "sway/workspaces" = {
+            disable-scroll = true;
+            all-outputs = true;
+          };
+          "custom/hello-from-waybar" = {
+            format = "hello {}";
+            max-length = 40;
+            interval = "once";
+            exec = pkgs.writeShellScript "hello-from-waybar" ''
+              echo "from within waybar"
+            '';
+          };
+        };
+      }];
+    };
 }
