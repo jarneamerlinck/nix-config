9f9f031080e7f7f2dfd6df7217028d10f3a8851b
jarneamerlinck <jarneamerlinck@pm.me>
2024-08-15 15:49:41 +0200

    Add color schemes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,17 @@
   sddm-themes = pkgs.callPackage ./sddm-themes.nix { };
   grub-themes = pkgs.callPackage ./grub-themes.nix { };
   hyprslurp = pkgs.callPackage ./hyprslurp { };
+
+  # Wallpapers
   wallpapers = import ./wallpapers {inherit pkgs;};
   allWallpapers = pkgs.linkFarmFromDrvs "wallpapers" (pkgs.lib.attrValues wallpapers);
 
+  # Color
+  generateColorscheme = import ./colorschemes/generator.nix {inherit pkgs;};
+  colorschemes = import ./colorschemes {inherit pkgs wallpapers generateColorscheme;};
+  allColorschemes = let
+    # This is here to help us keep IFD cached (hopefully)
+    combined = pkgs.writeText "colorschemes.json" (builtins.toJSON (pkgs.lib.mapAttrs (_: drv: drv.imported) colorschemes));
+  in
+    pkgs.linkFarmFromDrvs "colorschemes" (pkgs.lib.attrValues colorschemes ++ [combined]);
 }
