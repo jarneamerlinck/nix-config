5d679ad9d2ff53681ac0cfa6faccd70617f25ed4
jarneamerlinck <jarneamerlinck@pm.me>
2024-08-15 17:48:53 +0200

    updated new waybar config
    removed part

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -1,4 +1,11 @@
-{ config, lib, pkgs, inputs,  ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  outputs,
+  ...
+}:
 let
   rmHash = lib.removePrefix "#";
   inherit (config.colorscheme) colors harmonized;
@@ -23,6 +30,31 @@ let
   pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
   wofi = "${pkgs.wofi}/bin/wofi";
 
+  # Function to simplify making waybar outputs
+  jsonOutput =
+    name:
+    {
+      pre ? "",
+      text ? "",
+      tooltip ? "",
+      alt ? "",
+      class ? "",
+      percentage ? "",
+    }:
+    "${pkgs.writeShellScriptBin "waybar-${name}" ''
+      set -euo pipefail
+      ${pre}
+      ${jq} -cn \
+        --arg text "${text}" \
+        --arg tooltip "${tooltip}" \
+        --arg alt "${alt}" \
+        --arg class "${class}" \
+        --arg percentage "${percentage}" \
+        '{text:$text,tooltip:$tooltip,alt:$alt,class:$class,percentage:$percentage}'
+    ''}/bin/waybar-${name}";
+
+  hasSway = config.wayland.windowManager.sway.enable;
+  sway = config.wayland.windowManager.sway.package;
 in
 {
   programs.wofi = {
@@ -41,86 +73,278 @@ in
 
   programs.waybar = {
     enable = true;
+    package = pkgs.waybar.overrideAttrs (oa: {
+      mesonFlags = (oa.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
+    });
     systemd.enable = true;
-    style =
-      let
-        inherit (inputs.nix-colors.lib.conversions) hexToRGBString;
-        inherit (config.colorscheme) colors;
-        toRGBA = color: opacity: "rgba(${hexToRGBString "," color},${opacity})";
-      in
-      # css
-      ''
-        * {
-          font-family: ${config.fontProfiles.regular.family}, ${config.fontProfiles.monospace.family};
-          font-size: 12pt;
-          padding: 0;
-          margin: 0 0.4em;
-        }
+    settings = {
+      primary = {
+        mode = "dock";
+        layer = "top";
+        height = 40;
+        margin = "6";
+        position = "top";
+        modules-left =
+          [ "custom/menu" ]
+          ++ (lib.optionals hasSway [
+            "sway/workspaces"
+            "sway/mode"
+          ])
+          ++ [
+            "custom/currentplayer"
+            "custom/player"
+          ];
 
-        window#waybar {
-          padding: 0;
-          border-radius: 0.5em;
-          background-color: ${toRGBA c.palette.base00 "0.7"};
-          color: #${c.palette.base05};
-        }
-        .modules-left {
-          margin-left: -0.65em;
-        }
-        .modules-right {
-          margin-right: -0.65em;
-        }
+        modules-center = [
+          "cpu"
+          "custom/gpu"
+          "memory"
+          "clock"
+          "pulseaudio"
+          "battery"
+          "custom/unread-mail"
+          "custom/gpg-agent"
+        ];
 
-        #workspaces button {
-          background-color: #${c.palette.base00};
-          color: #${c.palette.base05};
-          padding-left: 0.4em;
-          padding-right: 0.4em;
-          margin-top: 0.15em;
-          margin-bottom: 0.15em;
-        }
-        #workspaces button.hidden {
-          background-color: #${c.palette.base00};
-          color: #${c.palette.base04};
-        }
-        #workspaces button.focused,
-        #workspaces button.active {
-          background-color: #${c.palette.base0A};
-          color: #${c.palette.base00};
-        }
+        modules-right = [
+          # "custom/gammastep" TODO: currently broken for some reason
+          "custom/tailscale-ping"
+          "network"
+          "tray"
+          "custom/hostname"
+        ];
 
-        #clock {
-          padding-right: 1em;
-          padding-left: 1em;
-          border-radius: 0.5em;
-        }
+        clock = {
+          interval = 1;
+          format = "{:%d/%m %H:%M:%S}";
+          format-alt = "{:%Y-%m-%d %H:%M:%S %z}";
+          on-click-left = "mode";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
 
-        #custom-menu {
-          background-color: #${c.palette.base01};
-          padding-right: 1.5em;
-          padding-left: 1em;
-          margin-right: 0;
-          border-radius: 0.5em;
-        }
-        #custom-menu.fullscreen {
-          background-color: #${c.palette.base0C};
-          color: #${c.palette.base00};
-        }
-        #custom-hostname {
-          padding-right: 1em;
-          padding-left: 1em;
-          margin-left: 0;
-          border-radius: 0.5em;
-        }
-        #custom-currentplayer {
-          padding-right: 0;
-        }
-        #tray {
-          color: #${c.palette.base05};
-        }
-        #custom-gpu, #cpu, #memory {
-          margin-left: 0.05em;
-          margin-right: 0.55em;
-        }
-      '';
+        cpu = {
+          format = "  {usage}%";
+        };
+        "custom/gpu" = {
+          interval = 5;
+          exec = "${cat} /sys/class/drm/card0/device/gpu_busy_percent";
+          format = "󰒋  {}%";
+        };
+        memory = {
+          format = "  {}%";
+          interval = 5;
+        };
+
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          format-muted = "   0%";
+          format-icons = {
+            headphone = "󰋋";
+            headset = "󰋎";
+            portable = "";
+            default = [
+              ""
+              ""
+              ""
+            ];
+          };
+          on-click = pavucontrol;
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "󰒳";
+            deactivated = "󰒲";
+          };
+        };
+        battery = {
+          bat = "BAT0";
+          interval = 10;
+          format-icons = [
+            "󰁺"
+            "󰁻"
+            "󰁼"
+            "󰁽"
+            "󰁾"
+            "󰁿"
+            "󰂀"
+            "󰂁"
+            "󰂂"
+            "󰁹"
+          ];
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          onclick = "";
+        };
+        "sway/window" = {
+          max-length = 20;
+        };
+        network = {
+          interval = 3;
+          format-wifi = "   {essid}";
+          format-ethernet = "󰈁 Connected";
+          format-disconnected = "";
+          tooltip-format = ''
+            {ifname}
+            {ipaddr}/{cidr}
+            Up: {bandwidthUpBits}
+            Down: {bandwidthDownBits}'';
+          on-click = "";
+        };
+        "custom/tailscale-ping" = {
+          interval = 10;
+          return-type = "json";
+          exec =
+            let
+              inherit (builtins) concatStringsSep attrNames;
+              hosts = attrNames outputs.nixosConfigurations;
+              homeMachine = "merope";
+              remoteMachine = "alcyone";
+            in
+            jsonOutput "tailscale-ping" {
+              # Build variables for each host
+              pre = ''
+                set -o pipefail
+                ${concatStringsSep "\n" (
+                  map (host: ''
+                    ping_${host}="$(${timeout} 2 ${ping} -c 1 -q ${host} 2>/dev/null | ${tail} -1 | ${cut} -d '/' -f5 | ${cut} -d '.' -f1)ms" || ping_${host}="Disconnected"
+                  '') hosts
+                )}
+              '';
+              # Access a remote machine's and a home machine's ping
+              text = "  $ping_${remoteMachine} /  $ping_${homeMachine}";
+              # Show pings from all machines
+              tooltip = concatStringsSep "\n" (map (host: "${host}: $ping_${host}") hosts);
+            };
+          format = "{}";
+          on-click = "";
+        };
+        "custom/menu" =
+          let
+            isFullScreen = "false";
+          in
+          {
+            interval = 1;
+            return-type = "json";
+            exec = jsonOutput "menu" {
+              text = "";
+              tooltip = ''$(${cat} /etc/os-release | ${grep} PRETTY_NAME | ${cut} -d '"' -f2)'';
+              class = "$(if ${isFullScreen}; then echo fullscreen; fi)";
+            };
+            on-click-left = "${wofi} -S drun -x 10 -y 10 -W 25% -H 60%";
+            on-click-right = lib.concatStringsSep ";" (
+              (lib.optional hasSway "${sway}/bin/swaymsg scratchpad show")
+            );
+          };
+        "custom/hostname" = {
+          exec = "echo $USER@$HOSTNAME";
+          on-click = "${systemctl} --user restart waybar";
+        };
+        "custom/unread-mail" = {
+          interval = 5;
+          return-type = "json";
+          exec = jsonOutput "unread-mail" {
+            pre = ''
+              count=$(${find} ~/Mail/*/Inbox/new -type f | ${wc} -l)
+              if ${pgrep} mbsync &>/dev/null; then
+                status="syncing"
+              else if [ "$count" == "0" ]; then
+                status="read"
+              else
+                status="unread"
+              fi
+              fi
+            '';
+            text = "$count";
+            alt = "$status";
+          };
+          format = "{icon}  ({})";
+          format-icons = {
+            "read" = "󰇯";
+            "unread" = "󰇮";
+            "syncing" = "󰁪";
+          };
+        };
+        "custom/gammastep" = {
+          interval = 5;
+          return-type = "json";
+          exec = jsonOutput "gammastep" {
+            pre = ''
+              if unit_status="$(${systemctl} --user is-active gammastep)"; then
+                status="$unit_status ($(${journalctl} --user -u gammastep.service -g 'Period: ' | ${tail} -1 | ${cut} -d ':' -f6 | ${xargs}))"
+              else
+                status="$unit_status"
+              fi
+            '';
+            alt = "\${status:-inactive}";
+            tooltip = "Gammastep is $status";
+          };
+          format = "{icon}";
+          format-icons = {
+            "activating" = "󰁪 ";
+            "deactivating" = "󰁪 ";
+            "inactive" = "? ";
+            "active (Night)" = " ";
+            "active (Nighttime)" = " ";
+            "active (Transition (Night)" = " ";
+            "active (Transition (Nighttime)" = " ";
+            "active (Day)" = " ";
+            "active (Daytime)" = " ";
+            "active (Transition (Day)" = " ";
+            "active (Transition (Daytime)" = " ";
+          };
+          on-click = "${systemctl} --user is-active gammastep && ${systemctl} --user stop gammastep || ${systemctl} --user start gammastep";
+        };
+        "custom/currentplayer" = {
+          interval = 2;
+          return-type = "json";
+          exec = jsonOutput "currentplayer" {
+            pre = ''
+              player="$(${playerctl} status -f "{{playerName}}" 2>/dev/null || echo "No player active" | ${cut} -d '.' -f1)"
+              count="$(${playerctl} -l 2>/dev/null | ${wc} -l)"
+              if ((count > 1)); then
+                more=" +$((count - 1))"
+              else
+                more=""
+              fi
+            '';
+            alt = "$player";
+            tooltip = "$player ($count available)";
+            text = "$more";
+          };
+          format = "{icon}{}";
+          format-icons = {
+            "No player active" = " ";
+            "Celluloid" = "󰎁 ";
+            "spotify" = "󰓇 ";
+            "ncspot" = "󰓇 ";
+            "qutebrowser" = "󰖟 ";
+            "firefox" = " ";
+            "discord" = " 󰙯 ";
+            "sublimemusic" = " ";
+            "kdeconnect" = "󰄡 ";
+            "chromium" = " ";
+          };
+          on-click = "${playerctld} shift";
+          on-click-right = "${playerctld} unshift";
+        };
+        "custom/player" = {
+          exec-if = "${playerctl} status 2>/dev/null";
+          exec = ''${playerctl} metadata --format '{"text": "{{title}} - {{artist}}", "alt": "{{status}}", "tooltip": "{{title}} - {{artist}} ({{album}})"}' 2>/dev/null '';
+          return-type = "json";
+          interval = 2;
+          max-length = 30;
+          format = "{icon} {}";
+          format-icons = {
+            "Playing" = "󰐊";
+            "Paused" = "󰏤 ";
+            "Stopped" = "󰓛";
+          };
+          on-click = "${playerctl} play-pause";
+        };
+      };
+    };
   };
 }
