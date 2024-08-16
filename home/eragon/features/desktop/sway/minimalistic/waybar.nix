01b7d55da914f6f9de76cc6426e974902463932a
jarneamerlinck <jarneamerlinck@pm.me>
2024-08-16 09:39:48 +0200

    updates waybar

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -89,11 +89,7 @@ in
           ++ (lib.optionals hasSway [
             "sway/workspaces"
             "sway/mode"
-          ])
-          ++ [
-            "custom/currentplayer"
-            "custom/player"
-          ];
+          ]);
 
         modules-center = [
           "cpu"
@@ -102,13 +98,9 @@ in
           "clock"
           "pulseaudio"
           "battery"
-          "custom/unread-mail"
-          "custom/gpg-agent"
         ];
 
         modules-right = [
-          # "custom/gammastep" TODO: currently broken for some reason
-          "custom/tailscale-ping"
           "network"
           "tray"
           "custom/hostname"
@@ -193,34 +185,6 @@ in
             Down: {bandwidthDownBits}'';
           on-click = "";
         };
-        "custom/tailscale-ping" = {
-          interval = 10;
-          return-type = "json";
-          exec =
-            let
-              inherit (builtins) concatStringsSep attrNames;
-              hosts = attrNames outputs.nixosConfigurations;
-              homeMachine = "merope";
-              remoteMachine = "alcyone";
-            in
-            jsonOutput "tailscale-ping" {
-              # Build variables for each host
-              pre = ''
-                set -o pipefail
-                ${concatStringsSep "\n" (
-                  map (host: ''
-                    ping_${host}="$(${timeout} 2 ${ping} -c 1 -q ${host} 2>/dev/null | ${tail} -1 | ${cut} -d '/' -f5 | ${cut} -d '.' -f1)ms" || ping_${host}="Disconnected"
-                  '') hosts
-                )}
-              '';
-              # Access a remote machine's and a home machine's ping
-              text = "  $ping_${remoteMachine} /  $ping_${homeMachine}";
-              # Show pings from all machines
-              tooltip = concatStringsSep "\n" (map (host: "${host}: $ping_${host}") hosts);
-            };
-          format = "{}";
-          on-click = "";
-        };
         "custom/menu" =
           let
             isFullScreen = "false";
@@ -240,7 +204,7 @@ in
           };
         "custom/hostname" = {
           exec = "echo $USER@$HOSTNAME";
-          on-click = "${systemctl} --user restart waybar";
+          # on-click = "${systemctl} --user restart waybar";
         };
         "custom/unread-mail" = {
           interval = 5;
@@ -266,83 +230,6 @@ in
             "unread" = "󰇮";
             "syncing" = "󰁪";
           };
-        };
-        "custom/gammastep" = {
-          interval = 5;
-          return-type = "json";
-          exec = jsonOutput "gammastep" {
-            pre = ''
-              if unit_status="$(${systemctl} --user is-active gammastep)"; then
-                status="$unit_status ($(${journalctl} --user -u gammastep.service -g 'Period: ' | ${tail} -1 | ${cut} -d ':' -f6 | ${xargs}))"
-              else
-                status="$unit_status"
-              fi
-            '';
-            alt = "\${status:-inactive}";
-            tooltip = "Gammastep is $status";
-          };
-          format = "{icon}";
-          format-icons = {
-            "activating" = "󰁪 ";
-            "deactivating" = "󰁪 ";
-            "inactive" = "? ";
-            "active (Night)" = " ";
-            "active (Nighttime)" = " ";
-            "active (Transition (Night)" = " ";
-            "active (Transition (Nighttime)" = " ";
-            "active (Day)" = " ";
-            "active (Daytime)" = " ";
-            "active (Transition (Day)" = " ";
-            "active (Transition (Daytime)" = " ";
-          };
-          on-click = "${systemctl} --user is-active gammastep && ${systemctl} --user stop gammastep || ${systemctl} --user start gammastep";
-        };
-        "custom/currentplayer" = {
-          interval = 2;
-          return-type = "json";
-          exec = jsonOutput "currentplayer" {
-            pre = ''
-              player="$(${playerctl} status -f "{{playerName}}" 2>/dev/null || echo "No player active" | ${cut} -d '.' -f1)"
-              count="$(${playerctl} -l 2>/dev/null | ${wc} -l)"
-              if ((count > 1)); then
-                more=" +$((count - 1))"
-              else
-                more=""
-              fi
-            '';
-            alt = "$player";
-            tooltip = "$player ($count available)";
-            text = "$more";
-          };
-          format = "{icon}{}";
-          format-icons = {
-            "No player active" = " ";
-            "Celluloid" = "󰎁 ";
-            "spotify" = "󰓇 ";
-            "ncspot" = "󰓇 ";
-            "qutebrowser" = "󰖟 ";
-            "firefox" = " ";
-            "discord" = " 󰙯 ";
-            "sublimemusic" = " ";
-            "kdeconnect" = "󰄡 ";
-            "chromium" = " ";
-          };
-          on-click = "${playerctld} shift";
-          on-click-right = "${playerctld} unshift";
-        };
-        "custom/player" = {
-          exec-if = "${playerctl} status 2>/dev/null";
-          exec = ''${playerctl} metadata --format '{"text": "{{title}} - {{artist}}", "alt": "{{status}}", "tooltip": "{{title}} - {{artist}} ({{album}})"}' 2>/dev/null '';
-          return-type = "json";
-          interval = 2;
-          max-length = 30;
-          format = "{icon} {}";
-          format-icons = {
-            "Playing" = "󰐊";
-            "Paused" = "󰏤 ";
-            "Stopped" = "󰓛";
-          };
-          on-click = "${playerctl} play-pause";
         };
       };
     };
