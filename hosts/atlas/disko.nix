f550de6ef3492ad53593c8b675ba7865f2cf18cf
jarneamerlinck <jarneamerlinck@pm.me>
2024-08-17 16:27:37 +0200

    refacter

--- hosts/atlas/disko.nix
+++ hosts/atlas/disko.nix
@@ -2,32 +2,33 @@
 { lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      disk1 = {
+        device = lib.mkDefault "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
             };
-          };
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
             root = {
               size = "100%";
               content = {
                 type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
+                extraArgs = [ "-f" ];
                 mountpoint = "/";
                 mountOptions = [ "compress=zstd" "noatime" ];
               };
@@ -35,5 +36,6 @@
           };
         };
       };
+    };
   };
 }
