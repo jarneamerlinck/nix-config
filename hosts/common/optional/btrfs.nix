868de814381265eb3522f53502b51a8481a1b0bf
jarneamerlinck <jarneamerlinck@pm.me>
2024-09-08 14:07:33 +0200

    added snapper root

--- hosts/common/optional/btrfs.nix
+++ hosts/common/optional/btrfs.nix
@@ -4,6 +4,23 @@
     snapshotInterval = "hourly";
     cleanupInterval = "1d";
     configs = {
+      root = {
+        SUBVOLUME = "/";
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_HOURLY = "10";
+        TIMELINE_LIMIT_DAILY = "7";
+        TIMELINE_LIMIT_WEEKLY = "0";
+        TIMELINE_LIMIT_MONTHLY = "0";
+        TIMELINE_LIMIT_YEARLY = "0";
+        BACKGROUND_COMPARISON = "yes";
+        NUMBER_CLEANUP = "no";
+        NUMBER_MIN_AGE = "1800";
+        NUMBER_LIMIT = "50";
+        NUMBER_LIMIT_IMPORTANT = "10";
+        EMPTY_PRE_POST_CLEANUP = "yes";
+        EMPTY_PRE_POST_MIN_AGE = "1800";
+      };
       log = {
         SUBVOLUME = "/var/log";
         TIMELINE_CREATE = true;
@@ -40,4 +57,6 @@
       };
     };
   };
+
+
 }
