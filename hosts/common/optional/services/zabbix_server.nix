98069c5c1c43582098728e1b01471d2095c82fe0
jarneamerlinck <jarneamerlinck@pm.me>
2024-09-25 20:42:51 +0200

    removed zabbix web

--- hosts/common/optional/services/zabbix_server.nix
+++ hosts/common/optional/services/zabbix_server.nix
@@ -4,30 +4,4 @@
     enable = true;
     openFirewall = true;
   };
-
-  services.nginx = {
-    enable = true;
-    virtualHosts = [
-      {
-        serverName = "zabbix.local";
-        listen = [ { addr = "0.0.0.0"; port = 31443; } ];
-        root = "${pkgs.zabbix.web}/share/zabbix/";
-        extraConfig = ''
-          location / {
-            try_files $uri $uri/ =404;
-          }
-        '';
-      }
-    ];
-
-    # # Ensure no default server listens on port 80
-    # defaultServer = {
-    #   listen = [ { addr = "0.0.0.0"; port = 80; } ];
-    #   extraConfig = ''
-    #     return 404;
-    #   '';
-    # };
-  };
-
-  networking.firewall.allowedTCPPorts = [ 31443 ];
 }
