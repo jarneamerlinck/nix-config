da7d3d0afe4000f9ecfcd8f68476ba33a3c55eb5
jarneamerlinck <jarneamerlinck@pm.me>
2024-09-25 19:42:15 +0200

    try other port

--- hosts/common/optional/services/zabbix_server.nix
+++ hosts/common/optional/services/zabbix_server.nix
@@ -1,17 +1,33 @@
 { pkgs, lib, config, inputs, ... }:
 {
   services.zabbixServer = {
-    enable= true;
-    openFirewall = true;
-
-  };
-  # networking.firewall.allowedTCPPorts = [ 80 443 ];
-  services.zabbixWeb = {
     enable = true;
-    virtualHost = {
-      hostName = "zabbix.local";
-      adminAddr = "webmaster@localhost";
-    };
+    openFirewall = true;
   };
 
+  services.nginx = {
+    enable = true;
+    virtualHosts = [
+      {
+        serverName = "zabbix.local";
+        listen = [ { addr = "0.0.0.0"; port = 31443; } ];
+        root = "${pkgs.zabbix.web}/share/zabbix/";
+        extraConfig = ''
+          location / {
+            try_files $uri $uri/ =404;
+          }
+        '';
+      }
+    ];
+
+    # # Ensure no default server listens on port 80
+    # defaultServer = {
+    #   listen = [ { addr = "0.0.0.0"; port = 80; } ];
+    #   extraConfig = ''
+    #     return 404;
+    #   '';
+    # };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 31443 ];
 }
