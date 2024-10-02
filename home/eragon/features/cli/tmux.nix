e63ee33d719e29a4d0a3af13e9d3a9c10801838d
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-02 20:54:06 +0200

    first try for packags

--- home/eragon/features/cli/tmux.nix
+++ home/eragon/features/cli/tmux.nix
@@ -1,7 +1,35 @@
 { outputs, lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    tmux
-  ];
+  home = {
+    packages = with pkgs; [
+      tmux
+    ];
+    shellAliases = {
+      t="tmux";
+    };
+  };
+  # programs.tmux = {
+  #   enable = true;
+  #   shell = "${pkgs.bash}/bin/bash";
+  #   clock24 = true;
+  #   terminal="screen-256color";
+  #   # plugins = with pkgs; [
+  #   #   tmuxPlugins.cpu
+  #   #   {
+  #   #     plugin = tmuxPlugins.resurrect;
+  #   #     extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+  #   #   }
+  #   #   {
+  #   #     plugin = tmuxPlugins.continuum;
+  #   #     extraConfig = ''
+  #   #       set -g @continuum-restore 'on'
+  #   #       set -g @continuum-save-interval '60' # minutes
+  #   #     '';
+  #   #   }
+  #   # ];
+  #
+  #
+  # };
+
 
 }
