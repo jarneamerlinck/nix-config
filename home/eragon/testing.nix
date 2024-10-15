606699faec7ec581d056ac3895b8db7ff05e63de
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-15 20:58:03 +0200

    added new vm host to test with

---
+++ home/eragon/testing.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, inputs,  ... }:
+{
+  imports = [
+    ./common
+    ./features/cli/debugging.nix
+    ./features/desktop/sway/minimalistic
+    ./features/music
+    ./features/cyber
+  ];
+  wallpaper = pkgs.wallpapers.nixos-logo;
+
+  colorScheme = inputs.nix-colors.colorSchemes.brewer;
+
+  monitors = [{
+    name = "eDP-1";
+    width = 1600;
+    height = 900;
+    workspace = "1";
+    primary = true;
+  }];
+
+}
