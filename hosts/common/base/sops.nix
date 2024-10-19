22d1d839f85ee2a85ee00f1c2a8b4ad5d6cbb700
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-19 17:14:06 +0200

    add packages for sops

--- hosts/common/base/sops.nix
+++ hosts/common/base/sops.nix
@@ -1,12 +1,18 @@
 {
   inputs,
   config,
+  pkgs,
   ...
 }: let
   isEd25519 = k: k.type == "ed25519";
   getKeyPath = k: k.path;
   keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
 in {
+  environment.systemPackages = with pkgs; [
+    sops
+    age
+  ];
+
   imports = [inputs.sops-nix.nixosModules.sops];
 
   sops = {
