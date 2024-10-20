033ebbf0fd4496f5476d22fc5404d80974f842a2
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-20 22:08:25 +0200

    looping networks

--- hosts/common/optional/wireless.nix
+++ hosts/common/optional/wireless.nix
@@ -6,34 +6,31 @@
     sopsFile = ../secrets.yml;
     neededForUsers = true;
   };
+  wifi_networks = ["home" "iotHome"];
+
 in  {
-  # Wireless secrets stored through sops
-  # sops.secrets.wireless_kightofzero = {
-  #   sopsFile = ../secrets.yml;
-  #   neededForUsers = true;
-  # };
-  sops.secrets."wireless/home/ssid" = {sopsFile = ../secrets.yml;neededForUsers = true;};
-  sops.secrets."wireless/home/psw" = sops_settings;
-  sops.secrets."wireless/iotHome/ssid" = {sopsFile = ../secrets.yml;neededForUsers = true;};
-  sops.secrets."wireless/iotHome/psw" = sops_settings;
-  # environment.etc."wireless-debug.txt".text =  toString config.sops.secrets.wireless.path;
-  # environment.etc."wireless-debug.txt".text =  toString config.sops.secrets.wireless.path;
+
+  sops.secrets = builtins.listToAttrs (
+    (map (wifi: {
+      name = "wireless/${wifi}/ssid";
+      value = sops_settings;
+    }) wifi_networks) ++
+    (map (wifi: {
+      name = "wireless/${wifi}/psw";
+      value = sops_settings;
+    }) wifi_networks)
+  );
+
   networking.networkmanager.enable = false;
   networking.wireless = {
     enable = true;
-    # networks = map (n: {
-    #   ssid = n.ssid;
-    #   psk = n.psw;
-    # }) (builtins.fromJSON config.sops.secrets.wireless);
-    # networks.home = {
-    #   ssid = config.sops.secrets.wireless.ssid;
-    #   psk = config.sops.secrets.wireless.kightofzero;
-    # };
+    networks = builtins.listToAttrs (map (wifi: {
+      name = config.sops.secrets."wireless/${wifi}/ssid".path;  # Dynamically get the SSID path
+      value = {
+        pskRaw = config.sops.secrets."wireless/${wifi}/psw".path;  # Dynamically get the PSK path
+      };
+    }) wifi_networks);
 
-    # networks.iothome = {
-    #   ssid = config.sops.secrets.wireless[1].iothome.SSID;
-    #   psk = config.sops.secrets.wireless[1].iothome.PSW;
-    # };
     # Imperative
     allowAuxiliaryImperativeNetworks = true;
     userControlled = {
