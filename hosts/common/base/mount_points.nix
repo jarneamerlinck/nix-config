640030d84f124400bf3aac19fb4b4ef5428dd31e
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 12:27:32 +0100

    added mount to wd decrypt

--- hosts/common/base/mount_points.nix
+++ hosts/common/base/mount_points.nix
@@ -7,8 +7,22 @@
 
     serviceConfig = {
       ExecStart = "${pkgs.coreutils}/bin/mkdir -p /mnt";
-      ExecStartPost = "${pkgs.coreutils}/bin/chmod 0755 /mnt"; # Set permissions
+      ExecStartPost = "${pkgs.coreutils}/bin/chmod 0755 /mnt";
       Type = "oneshot";
       RemainAfterExit = true;
     };
-  };}
+  };
+
+  systemd.services.ensureWDMntDir = {
+    description = "Ensure /mnt/wd directory exists";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.coreutils}/bin/mkdir -p /mnt/wd";
+      ExecStartPost = "${pkgs.coreutils}/bin/chmod 0755 /mnt/wd";
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
+}
--- hosts/common/optional/wd-decrypt.nix
+++ hosts/common/optional/wd-decrypt.nix
@@ -2,6 +2,7 @@
 let
   sops_decrypt_key = "wd-decrypt";
   sops_device_key = "wd-device-id";
+  sops_device_partition = "wd-device-decrypted";
 in
 {
 
@@ -14,10 +15,16 @@ in
    sopsFile = ../users/eragon/secrets.yml;
    neededForUsers = true;
  };
+
+ sops.secrets."${sops_device_partition}" = {
+   sopsFile = ../users/eragon/secrets.yml;
+   neededForUsers = true;
+ };
+
   environment.systemPackages = with pkgs; [
     sg3_utils
   ];
   environment.interactiveShellInit = ''
-  alias wd_attach='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00'
+  alias wd_attach='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00 &&  sudo mount /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_partition}".path}) /mnt/wd && ${pkgs.coreutils}/bin/chgpr -R users /mnt/wd && ${pkgs.coreutils}/bin/chmod -R 770 /mnt/wd'
 '';
 }
