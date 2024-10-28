26f437ecc3211b8c8a3302a620a25fc789e2195b
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 14:12:12 +0100

    mount wd with filesystem

--- hosts/common/optional/wd-decrypt.nix
+++ hosts/common/optional/wd-decrypt.nix
@@ -2,13 +2,14 @@
 let
   sops_decrypt_key = "wd-decrypt";
   sops_device_key = "wd-device-id";
-  sops_device_partition = "wd-device-decrypted";
+  device_partition = "wwn-0x50014ee6094bb38a-part1";
 in
 {
 
   environment.systemPackages = with pkgs; [
     sg3_utils
   ];
+
   services.devmon.enable = true;
   services.gvfs.enable = true;
   services.udisks2.enable = true;
@@ -23,16 +24,17 @@ in
     neededForUsers = true;
   };
 
-  sops.secrets."${sops_device_partition}" = {
-    sopsFile = ../users/eragon/secrets.yml;
-    neededForUsers = true;
+
+  fileSystems."/mnt/wd" = {
+    device = "/dev/disk/by-id/${device_partition}";
+    fsType = "ext4";
+    options = [ "noatime" "nofail" "noauto"];
   };
-
-
 
   environment.interactiveShellInit = ''
   alias wd_decrypt='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00'
-  alias wd_mount='sudo mount -t ext4 -o umask=770,gid=100 /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_partition}".path}) /mnt/wd'
+  alias wd_mount='sudo mount /mnt/wd'
   alias wd_attach='wd_decrypt && sleep 3 && wd_mount'
 '';
+
 }
