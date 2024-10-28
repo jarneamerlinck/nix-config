90f683006b9e6be9f297b7fae42ab5af19c3ea84
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 11:40:27 +0100

    added wd decrypt to sops

--- hosts/common/optional/wd-decrypt.nix
+++ hosts/common/optional/wd-decrypt.nix
@@ -1,12 +1,23 @@
 { pkgs, config, ... }:
+let
+  sops_decrypt_key = "wd-decrypt";
+  sops_device_key = "wd-device-id";
+in
 {
 
- sops.secrets."wd-decrypt" = {
+ sops.secrets."${sops_decrypt_key}" = {
    sopsFile = ../users/eragon/secrets.yml;
    neededForUsers = true;
  };
 
+ sops.secrets."${sops_device_key}" = {
+   sopsFile = ../users/eragon/secrets.yml;
+   neededForUsers = true;
+ };
   environment.systemPackages = with pkgs; [
     sg3_utils
   ];
+  environment.interactiveShellInit = ''
+  alias wd_attach='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00'
+'';
 }
