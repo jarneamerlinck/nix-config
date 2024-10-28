1d77f463e97f0d1db7125345d800d837628ce710
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 13:04:52 +0100

    added udisks2 for non sudo mount

--- hosts/common/optional/wd-decrypt.nix
+++ hosts/common/optional/wd-decrypt.nix
@@ -6,24 +6,31 @@ let
 in
 {
 
- sops.secrets."${sops_decrypt_key}" = {
-   sopsFile = ../users/eragon/secrets.yml;
-   neededForUsers = true;
- };
-
- sops.secrets."${sops_device_key}" = {
-   sopsFile = ../users/eragon/secrets.yml;
-   neededForUsers = true;
- };
-
- sops.secrets."${sops_device_partition}" = {
-   sopsFile = ../users/eragon/secrets.yml;
-   neededForUsers = true;
- };
-
   environment.systemPackages = with pkgs; [
     sg3_utils
   ];
+  services.udisks2 = {
+    enable = true;
+
+  };
+
+  sops.secrets."${sops_decrypt_key}" = {
+    sopsFile = ../users/eragon/secrets.yml;
+    neededForUsers = true;
+  };
+
+  sops.secrets."${sops_device_key}" = {
+    sopsFile = ../users/eragon/secrets.yml;
+    neededForUsers = true;
+  };
+
+  sops.secrets."${sops_device_partition}" = {
+    sopsFile = ../users/eragon/secrets.yml;
+    neededForUsers = true;
+  };
+
+
+
   environment.interactiveShellInit = ''
   alias wd_decrypt='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00'
   alias wd_mount='sudo mount -t ext4 -o umask=770,gid=100 /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_partition}".path}) /mnt/wd'
