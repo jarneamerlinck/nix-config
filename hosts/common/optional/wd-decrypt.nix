46554907c360e0b059eacd74ef63c5ef2f38e431
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 12:30:52 +0100

    add delay for decrypt

--- hosts/common/optional/wd-decrypt.nix
+++ hosts/common/optional/wd-decrypt.nix
@@ -25,6 +25,6 @@ in
     sg3_utils
   ];
   environment.interactiveShellInit = ''
-  alias wd_attach='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00 &&  sudo mount /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_partition}".path}) /mnt/wd && ${pkgs.coreutils}/bin/chgpr -R users /mnt/wd && ${pkgs.coreutils}/bin/chmod -R 770 /mnt/wd'
+  alias wd_attach='sudo sg_raw -s 40 -i /run/secrets-for-users/${sops_decrypt_key} /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_key}".path}) c1 e1 00 00 00 00 00 00 28 00 && sleep 2 && sudo mount /dev/disk/by-id/$(sudo cat ${config.sops.secrets."${sops_device_partition}".path}) /mnt/wd && ${pkgs.coreutils}/bin/chgpr -R users /mnt/wd && ${pkgs.coreutils}/bin/chmod -R 770 /mnt/wd'
 '';
 }
