0300703572ec59cbc00985a23af9ca7ef35f575e
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-30 22:55:53 +0100

    color vesktop
    rebuild

--- home/eragon/features/desktop/common/discord.nix
+++ home/eragon/features/desktop/common/discord.nix
@@ -15,26 +15,26 @@ in {
       @import url("https://slowstab.github.io/dracula/BetterDiscord/source.css");
       @import url("https://mulverinex.github.io/legacy-settings-icons/dist-native.css");
       .theme-dark, .theme-light, :root {
-        --text-default: ${c.base03};
-        --header-primary: ${c.base06};
-        --header-secondary: ${c.base07};
-        --channeltextarea-background: ${c.base08};
-        --interactive-normal: ${c.base06};
-        --interactive-active: ${c.base02};
+        --text-default: ${c.base04};
+        --header-primary: ${c.base01};
+        --header-secondary: ${c.base02};
+        --channeltextarea-background: ${c.base09};
+        --interactive-normal: ${c.base07};
+        --interactive-active: ${c.base03};
 
-        --dracula-primary: ${c.base00};
-        --dracula-secondary: ${c.base01};
-        --dracula-secondary-alpha: ${c.base02}ee;
-        --dracula-tertiary: ${c.base03};
-        --dracula-tertiary-alpha: ${c.base09}aa;
-        --dracula-primary-light: ${c.base0A};
+        --dracula-primary: ${c.base01};
+        --dracula-secondary: ${c.base02};
+        --dracula-secondary-alpha: ${c.base03}ee;
+        --dracula-tertiary: ${c.base04};
+        --dracula-tertiary-alpha: ${c.base0A}aa;
+        --dracula-primary-light: ${c.base0B};
 
-        --dracula-accent: ${c.base00};
-        --dracula-accent-alpha: ${c.base00}66;
-        --dracula-accent-alpha-alt: ${c.base01}88;
-        --dracula-accent-alpha-alt2: ${c.base02}aa;
-        --dracula-accent-dark: ${c.base04};
-        --dracula-accent-light: ${c.base05};
+        --dracula-accent: ${c.base01};
+        --dracula-accent-alpha: ${c.base01}66;
+        --dracula-accent-alpha-alt: ${c.base02}88;
+        --dracula-accent-alpha-alt2: ${c.base03}aa;
+        --dracula-accent-dark: ${c.base05};
+        --dracula-accent-light: ${c.base06};
       }
 
       html.theme-light #app-mount::after {
