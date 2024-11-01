b4283a9320a6dbbb982bc4da51852fc6e6a7a916
jarneamerlinck <jarneamerlinck@pm.me>
2024-11-01 10:28:47 +0100

    add qt themes

--- home/eragon/features/desktop/common/default.nix
+++ home/eragon/features/desktop/common/default.nix
@@ -8,16 +8,13 @@
 }:
 {
   imports = [
-    # ./deluge.nix
     ./discord.nix
     ./dragon.nix
     ./firefox.nix
     ./mediaPlayer.nix
     ./font.nix
     ./xdg.nix
-    # ./gtk.nix
-    # ./kdeconnect.nix
-    # ./pavucontrol.nix
+    ./qt.nix
 
   ];
   dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
