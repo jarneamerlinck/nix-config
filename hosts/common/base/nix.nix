e798653c7450d995b34faf1d723076b52dba93ef
jarneamerlinck <jarneamerlinck@pm.me>
2024-12-10 20:46:58 +0100

    fixes for 24.11

--- hosts/common/base/nix.nix
+++ hosts/common/base/nix.nix
@@ -15,7 +15,7 @@
       # ];
       trusted-users = [ "root" "@wheel" ];
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = [ "nix-command" "flakes" ];
       warn-dirty = false;
       system-features = [ "kvm" "big-parallel" "nixos-test" ];
       flake-registry = ""; # Disable global flake registry
