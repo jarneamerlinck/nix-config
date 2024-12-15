b7da19eb14cbe52eacedbff68172274360572fd3
jarneamerlinck <jarneamerlinck@pm.me>
2024-12-15 11:00:19 +0100

    cleaning up waybar config

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -2,30 +2,15 @@
   config,
   lib,
   pkgs,
-  inputs,
-  outputs,
   ...
 }:
 let
-  rmHash = lib.removePrefix "#";
   inherit (config.colorscheme) palette;
 
   cat = "${pkgs.coreutils}/bin/cat";
-  cut = "${pkgs.coreutils}/bin/cut";
-  find = "${pkgs.findutils}/bin/find";
-  grep = "${pkgs.gnugrep}/bin/grep";
-  pgrep = "${pkgs.procps}/bin/pgrep";
-  tail = "${pkgs.coreutils}/bin/tail";
-  wc = "${pkgs.coreutils}/bin/wc";
-  xargs = "${pkgs.findutils}/bin/xargs";
-  timeout = "${pkgs.coreutils}/bin/timeout";
-  ping = "${pkgs.iputils}/bin/ping";
 
   jq = "${pkgs.jq}/bin/jq";
   systemctl = "${pkgs.systemd}/bin/systemctl";
-  journalctl = "${pkgs.systemd}/bin/journalctl";
-  playerctl = "${pkgs.playerctl}/bin/playerctl";
-  playerctld = "${pkgs.playerctl}/bin/playerctld";
   pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
 
   # Function to simplify making waybar outputs
