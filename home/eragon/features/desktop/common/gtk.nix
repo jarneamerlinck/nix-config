f80a966b321700fc83540de3a93d424a90e5d812
jarneamerlinck <jarneamerlinck@pm.me>
2024-12-21 21:14:22 +0100

    fix gtk theme

--- home/eragon/features/desktop/common/gtk.nix
+++ home/eragon/features/desktop/common/gtk.nix
@@ -9,22 +9,22 @@ let
   colorPalette = ''
     /* GTK Theme generated from nix-colors */
 
-    @define-color base00 ${palette.base00};
-    @define-color base01 ${palette.base01};
-    @define-color base02 ${palette.base02};
-    @define-color base03 ${palette.base03};
-    @define-color base04 ${palette.base04};
-    @define-color base05 ${palette.base05};
-    @define-color base06 ${palette.base06};
-    @define-color base07 ${palette.base07};
-    @define-color base08 ${palette.base08};
-    @define-color base09 ${palette.base09};
-    @define-color base0A ${palette.base0A};
-    @define-color base0B ${palette.base0B};
-    @define-color base0C ${palette.base0C};
-    @define-color base0D ${palette.base0D};
-    @define-color base0E ${palette.base0E};
-    @define-color base0F ${palette.base0F};
+    @define-color base00 #${palette.base00};
+    @define-color base01 #${palette.base01};
+    @define-color base02 #${palette.base02};
+    @define-color base03 #${palette.base03};
+    @define-color base04 #${palette.base04};
+    @define-color base05 #${palette.base05};
+    @define-color base06 #${palette.base06};
+    @define-color base07 #${palette.base07};
+    @define-color base08 #${palette.base08};
+    @define-color base09 #${palette.base09};
+    @define-color base0A #${palette.base0A};
+    @define-color base0B #${palette.base0B};
+    @define-color base0C #${palette.base0C};
+    @define-color base0D #${palette.base0D};
+    @define-color base0E #${palette.base0E};
+    @define-color base0F #${palette.base0F};
 
     /* Example GTK styles leveraging the defined colors */
     * {
