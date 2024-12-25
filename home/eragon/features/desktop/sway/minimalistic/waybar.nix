1270d639a8f08d56cacd7a804a98e925e2cb1e56
jarneamerlinck <jarneamerlinck@pm.me>
2024-12-25 22:26:05 +0100

    Disable wofi not using theme
    As it kills the gtk theme of all other apps launched by wofi

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -215,7 +215,7 @@ in
         "custom/menu" =
           {
             exec = "echo ";
-            on-click = "exec GTK_THEME=none ${pkgs.wofi}/bin/wofi -S drun -x 10 -y 10 -W 25% -H 60%";
+            on-click = "exec ${pkgs.wofi}/bin/wofi -S drun -x 10 -y 10 -W 25% -H 60%";
           };
         "custom/hostname" = {
           exec = "echo $USER@$HOSTNAME";
