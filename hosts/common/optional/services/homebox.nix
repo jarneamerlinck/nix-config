c0a8fbdfa0afd23313b0e440cdb1ae38e2a3ea53
jarneamerlinck <jarneamerlinck@pm.me>
2024-12-28 14:57:13 +0100

    update homebox image

--- hosts/common/optional/services/homebox.nix
+++ hosts/common/optional/services/homebox.nix
@@ -4,7 +4,7 @@
 {
   # Runtime
   virtualisation.oci-containers.containers."homebox-homebox" = {
-    image = "ghcr.io/sysadminsmedia/homebox:0.15.2";
+    image = "ghcr.io/sysadminsmedia/homebox:0.16.0";
     environment = {
       "HBOX_LOG_FORMAT" = "text";
       "HBOX_LOG_LEVEL" = "info";
