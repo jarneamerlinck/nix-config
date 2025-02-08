c900dbc19e15468ac8f844c08848a8d30425087e
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-08 15:05:07 +0100

    try to build openlens

--- pkgs/openlens.nix
+++ pkgs/openlens.nix
@@ -1,27 +1,44 @@
-{ stdenv, fetchFromGitHub, nodejs, yarn, makeWrapper }:
+{ stdenv, fetchFromGitHub, nodejs, yarn, makeWrapper, electron, gcc, jq, curl, patchelf }:
 {
-  openlens = stdenv.mkDerivation rec {
+
+  openlens =  stdenv.mkDerivation rec {
     pname = "openlens";
-    version = "6.5.2-366";
-  
+    version = "6.5.2";
+
     src = fetchFromGitHub {
-      owner = "MuhammedKalkan";
-      repo = "OpenLens";
+      owner = "lensapp";
+      repo = "lens";
       rev = "v${version}";
       sha256 = "sha256-tg6XeeX5R3aZpmo+o9OJE/LciJVnBg36l6d7BE2LSM8=";
     };
   
-    buildInputs = [ nodejs yarn makeWrapper ];
+    # nativeBuildInputs = [ nodejs yarn makeWrapper gcc jq curl patchelf ];
+    buildInputs = [
+      electron
+      nodejs
+      yarn
+      gcc
+    ];
+  
+    # unpackPhase = "tar xvf ${src} --strip-components=1";
   
     buildPhase = ''
-      yarn install
-      yarn build
+      ls -lhi
+      cd open-lens
+      npm install
+      npm run all:install
+      cd ..
+  
+      mkdir -p open-lens/node_modules
+      npx nx run open-lens:build:app
     '';
   
     installPhase = ''
       mkdir -p $out/bin
-      cp -r build/* $out/bin
+      cp -r lens/open-lens/dist/* $out/bin
       ln -s $out/bin/openlens $out/bin/openlens-cli
     '';
+  
   };
 }
+
