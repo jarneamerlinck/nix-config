5688080ee39cd8ef049dbb26e3163cd101c62a39
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-08 14:30:50 +0100

    disable snapper

--- hosts/common/optional/btrfs.nix
+++ hosts/common/optional/btrfs.nix
@@ -1,44 +1,44 @@
 { lib,config,pkgs, ... }:
 {
-  services.snapper = {
-    snapshotInterval = "hourly";
-    configs = {
-      root = {
-        SUBVOLUME = "/";
-        TIMELINE_CREATE = true;
-        TIMELINE_CLEANUP = true;
-        TIMELINE_LIMIT_HOURLY = "10";
-        TIMELINE_LIMIT_DAILY = "7";
-        TIMELINE_LIMIT_WEEKLY = "0";
-        TIMELINE_LIMIT_MONTHLY = "0";
-        TIMELINE_LIMIT_YEARLY = "0";
-        BACKGROUND_COMPARISON = "yes";
-        NUMBER_CLEANUP = "no";
-        NUMBER_MIN_AGE = "1800";
-        NUMBER_LIMIT = "50";
-        NUMBER_LIMIT_IMPORTANT = "10";
-        EMPTY_PRE_POST_CLEANUP = "yes";
-        EMPTY_PRE_POST_MIN_AGE = "1800";
-      };
-      home = {
-        SUBVOLUME = "/home";
-        TIMELINE_CREATE = true;
-        TIMELINE_CLEANUP = true;
-        TIMELINE_LIMIT_HOURLY = "10";
-        TIMELINE_LIMIT_DAILY = "7";
-        TIMELINE_LIMIT_WEEKLY = "2";
-        TIMELINE_LIMIT_MONTHLY = "0";
-        TIMELINE_LIMIT_YEARLY = "0";
-        BACKGROUND_COMPARISON = "yes";
-        NUMBER_CLEANUP = "no";
-        NUMBER_MIN_AGE = "1800";
-        NUMBER_LIMIT = "50";
-        NUMBER_LIMIT_IMPORTANT = "10";
-        EMPTY_PRE_POST_CLEANUP = "yes";
-        EMPTY_PRE_POST_MIN_AGE = "1800";
-      };
-    };
-  };
+  # services.snapper = {
+  #   snapshotInterval = "hourly";
+  #   configs = {
+  #     root = {
+  #       SUBVOLUME = "/";
+  #       TIMELINE_CREATE = true;
+  #       TIMELINE_CLEANUP = true;
+  #       TIMELINE_LIMIT_HOURLY = "10";
+  #       TIMELINE_LIMIT_DAILY = "7";
+  #       TIMELINE_LIMIT_WEEKLY = "0";
+  #       TIMELINE_LIMIT_MONTHLY = "0";
+  #       TIMELINE_LIMIT_YEARLY = "0";
+  #       BACKGROUND_COMPARISON = "yes";
+  #       NUMBER_CLEANUP = "no";
+  #       NUMBER_MIN_AGE = "1800";
+  #       NUMBER_LIMIT = "50";
+  #       NUMBER_LIMIT_IMPORTANT = "10";
+  #       EMPTY_PRE_POST_CLEANUP = "yes";
+  #       EMPTY_PRE_POST_MIN_AGE = "1800";
+  #     };
+  #     home = {
+  #       SUBVOLUME = "/home";
+  #       TIMELINE_CREATE = true;
+  #       TIMELINE_CLEANUP = true;
+  #       TIMELINE_LIMIT_HOURLY = "10";
+  #       TIMELINE_LIMIT_DAILY = "7";
+  #       TIMELINE_LIMIT_WEEKLY = "2";
+  #       TIMELINE_LIMIT_MONTHLY = "0";
+  #       TIMELINE_LIMIT_YEARLY = "0";
+  #       BACKGROUND_COMPARISON = "yes";
+  #       NUMBER_CLEANUP = "no";
+  #       NUMBER_MIN_AGE = "1800";
+  #       NUMBER_LIMIT = "50";
+  #       NUMBER_LIMIT_IMPORTANT = "10";
+  #       EMPTY_PRE_POST_CLEANUP = "yes";
+  #       EMPTY_PRE_POST_MIN_AGE = "1800";
+  #     };
+  #   };
+  # };
 
 
 }
