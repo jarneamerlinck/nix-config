9fdd2c6ec8b8ba873310018b63b36ce5e33d25e7
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-08 12:20:14 +0100

    disable option waybar flags

--- home/eragon/features/desktop/sway/minimalistic/waybar.nix
+++ home/eragon/features/desktop/sway/minimalistic/waybar.nix
@@ -100,9 +100,6 @@ in
 {
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar.overrideAttrs (oa: {
-      mesonFlags = (oa.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
-    });
     systemd.enable = true;
     style = waybarStyle;
     settings = {
