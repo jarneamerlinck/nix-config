8cdb24c5d34933381421adf52b21339ef7cafb6a
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-09 15:54:39 +0100

    add prometheus as default for k3s

--- hosts/common/optional/virtualization/kubernetes/default.nix
+++ hosts/common/optional/virtualization/kubernetes/default.nix
@@ -1,6 +1,47 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-  ];
+  services.k3s = {
+
+    manifests.prometheus = {
+      enable = true;
+      content = [
+        # Namespace creation
+        {
+          apiVersion = "v1";
+          kind = "Namespace";
+          metadata.name = "monitoring";
+        }
+
+        # Hem repo
+        {
+          apiVersion = "helm.cattle.io/v1";
+          kind = "HelmRepository";
+          metadata = {
+            namespace = "kube-system";
+          };
+          spec.url = "https://prometheus-community.github.io/helm-charts";
+        }
+        # Helm chart for Prometheus
+        {
+          apiVersion = "helm.cattle.io/v1";
+          kind = "HelmChart";
+          metadata = {
+            name =  "prometheus-stack";
+            namespace = "kube-system";
+          };
+          spec = {
+            chart =  "prometheus-community/kube-prometheus-stack";
+            version = "69.2.0";
+            targetNamespace = "monitoring";
+            valuesContent = ''
+              grafana:
+                enabled: false
+              namespaceOverride: monitoring
+            '';
+          };
+        }
+      ];
+    };
+  };
 }
