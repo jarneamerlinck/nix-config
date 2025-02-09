6d3acaff27a50be2cf6747e914da2ada95a01940
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-09 13:08:33 +0100

    add k3s for single server

--- hosts/common/optional/virtualization/kubernetes/single_node.nix
+++ hosts/common/optional/virtualization/kubernetes/single_node.nix
@@ -3,9 +3,18 @@
   imports = [
     ./default.nix
   ];
-	services.k3s ={
-	
-		};
 
-
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString ([
+        "--write-kubeconfig-mode \"0644\""
+        "--cluster-init"
+        "--disable servicelb"
+        "--disable traefik"
+        "--disable local-storage"
+    ]);
+    clusterInit = true;
+    
+  };
 }
