82503e5fdae27d9eeefc8ed273709a898cd054e0
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-16 21:51:01 +0100

    change namespace for cert manager and haproxy

--- hosts/common/optional/virtualization/kubernetes/cert-manager.nix
+++ hosts/common/optional/virtualization/kubernetes/cert-manager.nix
@@ -5,19 +5,26 @@
     manifests."cert-manager" = {
       enable = lib.mkDefault true;
       content = [
+
+        # Namespace creation
+        {
+          apiVersion = "v1";
+          kind = "Namespace";
+          metadata.name = "certs";
+        }
         # Helm chart for Cert manager
         {
           apiVersion = "helm.cattle.io/v1";
           kind = "HelmChart";
           metadata = {
             name =  "cert-manager";
-            namespace = "kube-system";
+            namespace = "certs";
           };
           spec = {
             repo = "https://charts.jetstack.io";
             chart =  "cert-manager";
             version = "v1.17.1";
-            targetNamespace = "kube-system";
+            targetNamespace = "certs";
             valuesContent = ''
               crds:
                 enabled: true
--- hosts/common/optional/virtualization/kubernetes/haproxy.nix
+++ hosts/common/optional/virtualization/kubernetes/haproxy.nix
@@ -5,19 +5,26 @@
     manifests.haproxy = {
       enable = lib.mkDefault true;
       content = [
+
+        # Namespace creation
+        {
+          apiVersion = "v1";
+          kind = "Namespace";
+          metadata.name = "certs";
+        }
         # Helm chart for Haproxy
         {
           apiVersion = "helm.cattle.io/v1";
           kind = "HelmChart";
           metadata = {
             name =  "harpoxy-ingress";
-            namespace = "kube-system";
+            namespace = "certs";
           };
           spec = {
             repo = "https://haproxytech.github.io/helm-charts";
             chart =  "kubernetes-ingress";
             version = "1.44.0";
-            targetNamespace = "kube-system";
+            targetNamespace = "certs";
             valuesContent = ''
             '';
           };
