14350698130fbf31a156879e01a018e81beb6ffb
jarneamerlinck <jarneamerlinck@pm.me>
2025-02-21 21:35:15 +0100

    update open lens package

--- pkgs/openlens.nix
+++ pkgs/openlens.nix
@@ -1,42 +1,31 @@
 { stdenv, fetchFromGitHub, nodejs, yarn, makeWrapper, electron, gcc, jq, curl, patchelf, pkgs }:
 {
-
-  openlens =  stdenv.mkDerivation rec {
+  openlens = pkgs.stdenv.mkDerivation rec {
     pname = "openlens";
-    version = "6.5.2";
-
-    src = fetchFromGitHub {
-      owner = "lensapp";
-      repo = "lens";
-      rev = "v${version}";
-      sha256 = "sha256-9qxsaKYqDV5LLzVoIngGyTggbiOj2k8zmqqWsjlPP78=";
+    version = "6.5.2-366";
+    
+    src = pkgs.fetchurl {
+      url = "https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.5.2-366/OpenLens-6.5.2-366.x86_64.AppImage";
+      sha256 = "f17b841edd003c8a15c526b0897724595e18967475138b09290a90941f5de801"; 
     };
-  
-    nativeBuildInputs = [ nodejs yarn makeWrapper gcc jq curl patchelf ];
-    buildInputs = [
-      electron
-      pkgs.nodejs_18
-      yarn
-      gcc
-    ];
-  
-    # unpackPhase = "tar xvf ${src} --strip-components=1";
-  
-    buildPhase = ''
-      rm package-lock.json
-      npm install
-      npm run all:install
-  
-      mkdir -p open-lens/node_modules
-      npx nx run open-lens:build:app
-    '';
-  
+
+    dontUnpack = true;
+
     installPhase = ''
-      mkdir -p $out/bin
-      cp -r lens/open-lens/dist/* $out/bin
-      ln -s $out/bin/openlens $out/bin/openlens-cli
+      mkdir -p $out/bin $out/share/applications $out/share/icons/hicolor/128x128/apps
+      install -m 755 $src $out/bin/openlens
+      ln -s $out/bin/openlens $out/bin/OpenLens
+      
+      cat > $out/share/applications/openlens.desktop <<EOF
+      [Desktop Entry]
+      Name=OpenLens
+      Exec=$out/bin/openlens
+      Icon=openlens
+      Type=Application
+      Categories=Development;
+      EOF
     '';
-  
+
   };
 }
 
