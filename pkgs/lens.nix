b30db115995be096bef03fdf4b9e9227793f5582
jarneamerlinck <jarneamerlinck@pm.me>
2025-03-09 17:50:50 +0100

    more tries

--- pkgs/lens.nix
+++ pkgs/lens.nix
@@ -5,34 +5,38 @@
   fetchurl,
 }:
 let
-  version = "1.0.0";
+  pversion = "1.0.0";
   pname = "freelens";
 
-  src = fetchurl {
-    url = "https://github.com/freelensapp/freelens/releases/download/v${version}/Freelens-${version}-linux-amd64.AppImage";
-    hash = "sha256-Ic7Algr+8ZNNMp5WsaxbGhylkTwjpT/GABRnly4ghmw=";
-  };
+  # src = fetchurl {
+  #   url = "https://github.com/freelensapp/freelens/releases/download/v${version}/Freelens-${version}-linux-amd64.AppImage";
+  #   hash = "sha256-Ic7Algr+8ZNNMp5WsaxbGhylkTwjpT/GABRnly4ghmw=";
+  # };
 
-  appimageContents = appimageTools.extractType1 { inherit pname src; };
+  # appimageContents = appimageTools.extractType1 { inherit pname src; };
 in
 
   {
     freelens = appimageTools.wrapType2 rec {
-    inherit pname version src;
+      name= "${pname}"; 
+      version = "${pversion}";
+      src = fetchurl {
+        url = "https://github.com/freelensapp/freelens/releases/download/v${pversion}/Freelens-${pversion}-linux-amd64.AppImage";
+        hash = "sha256-Ic7Algr+8ZNNMp5WsaxbGhylkTwjpT/GABRnly4ghmw=";
+      };
+      # extraInstallCommands = ''
+      #   substituteInPlace $out/share/applications/${pname}.desktop \
+      #     --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
+      # '';
   
-    extraInstallCommands = ''
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
-    '';
-  
-    meta = {
-      description = "Free and open-source Kubernetes IDE";
-      homepage = "https://github.com/freelensapp/freelens";
-      downloadPage = "https://github.com/freelensapp/freelens/releases";
-      # license = lib.licenses.asl20;
-      sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-      # maintainers = with lib.maintainers; [ onny ];
-      platforms = [ "x86_64-linux" ];
-    };
+      meta = {
+        description = "Free and open-source Kubernetes IDE";
+        homepage = "https://github.com/freelensapp/freelens";
+        downloadPage = "https://github.com/freelensapp/freelens/releases";
+        # license = lib.licenses.asl20;
+        sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+        # maintainers = with lib.maintainers; [ onny ];
+        platforms = [ "x86_64-linux" ];
+      };
   };
 }
