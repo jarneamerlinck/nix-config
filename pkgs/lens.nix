593f453e134e86b802d8feebdbc83005cd23cf44
jarneamerlinck <jarneamerlinck@pm.me>
2025-03-09 17:57:43 +0100

    add icon

--- pkgs/lens.nix
+++ pkgs/lens.nix
@@ -17,38 +17,47 @@ let
 in
 
   {
-    freelens = appimageTools.wrapType2 rec {
-      name= "${pname}"; 
-      version = "${pversion}";
-      src = fetchurl {
-        url = "https://github.com/freelensapp/freelens/releases/download/v${pversion}/Freelens-${pversion}-linux-amd64.AppImage";
-        hash = "sha256-Ic7Algr+8ZNNMp5WsaxbGhylkTwjpT/GABRnly4ghmw=";
-      };
-      extraInstallCommands = ''
+  freelens = appimageTools.wrapType2 rec {
+    name= "${pname}"; 
+    version = "${pversion}";
+    src = fetchurl {
+      url = "https://github.com/freelensapp/freelens/releases/download/v${pversion}/Freelens-${pversion}-linux-amd64.AppImage";
+      hash = "sha256-Ic7Algr+8ZNNMp5WsaxbGhylkTwjpT/GABRnly4ghmw=";
+    };
 
-        mkdir -p $out/share/applications
-        cat > $out/share/applications/freelens.desktop <<EOF
-        [Desktop Entry]
-        Name=Freelens
-        Comment=Free and open-source Kubernetes IDE
-        Exec=${pname}
-        Icon=freelens
-        Terminal=false
-        Type=Application
-        Categories=Development;Kubernetes;
-        EOF
-      '';
+    icon = fetchurl {
+      url = "https://avatars.githubusercontent.com/u/172038998";
+      sha256 = "sha256-ayIriBfFiPCCpP059ieqG/1v+97e5aLYfAkOpC8PowM=";
+    };
+
+    extraInstallCommands = ''
+      mkdir -p $out/share/applications
+      cat > $out/share/applications/freelens.desktop <<EOF
+      [Desktop Entry]
+      Name=Freelens
+      Comment=Free and open-source Kubernetes IDE
+      Exec=${pname}
+      Icon=freelens
+      Terminal=false
+      Type=Application
+      Categories=Development;Kubernetes;
+      EOF
+
+
+      mkdir -p $out/share/icons/hicolor/scalable/apps
+      install -m 644 ${icon} $out/share/icons/hicolor/scalable/apps/freelens.png 
+    '';
 
 
   
-      meta = {
-        description = "Free and open-source Kubernetes IDE";
-        homepage = "https://github.com/freelensapp/freelens";
-        downloadPage = "https://github.com/freelensapp/freelens/releases";
-        # license = lib.licenses.asl20;
-        sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-        # maintainers = with lib.maintainers; [ onny ];
-        platforms = [ "x86_64-linux" ];
-      };
+    meta = {
+      description = "Free and open-source Kubernetes IDE";
+      homepage = "https://github.com/freelensapp/freelens";
+      downloadPage = "https://github.com/freelensapp/freelens/releases";
+      # license = lib.licenses.asl20;
+      sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+      # maintainers = with lib.maintainers; [ onny ];
+      platforms = [ "x86_64-linux" ];
+    };
   };
 }
