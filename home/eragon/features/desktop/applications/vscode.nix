f117ad7a0100e72b557cde848488cda01d91784f
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-03 19:30:47 +0200

    remapped vscode settings

--- home/eragon/features/desktop/applications/vscode.nix
+++ home/eragon/features/desktop/applications/vscode.nix
@@ -6,27 +6,29 @@ in {
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    userSettings = {
-      "workbench.colorCustomizations" = {
-        "editor.background" = "#${palette.base00}";
-        "editor.foreground" = "#${palette.base01}";
-        "activityBar.background" = "#${palette.base00}";
-        "sideBar.background" = "#${palette.base01}";
-        "statusBar.background" = "#${palette.base01}";
+    profiles.default = {
+      userSettings = {
+        "workbench.colorCustomizations" = {
+          "editor.background" = "#${palette.base00}";
+          "editor.foreground" = "#${palette.base01}";
+          "activityBar.background" = "#${palette.base00}";
+          "sideBar.background" = "#${palette.base01}";
+          "statusBar.background" = "#${palette.base01}";
 
 
-        # Menu bar dropdown items
-        "menu.background" = "#${palette.base02}";
-        "menu.foreground" = "#${palette.base01}";
-        "menu.selectionBackground" = "#${palette.base03}";
-        "menu.selectionForeground" = "#${palette.base01}";
+          # Menu bar dropdown items
+          "menu.background" = "#${palette.base02}";
+          "menu.foreground" = "#${palette.base01}";
+          "menu.selectionBackground" = "#${palette.base03}";
+          "menu.selectionForeground" = "#${palette.base01}";
+        };
       };
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        ms-toolsai.jupyter
+        njpwerner.autodocstring
+      ];
     };
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      ms-toolsai.jupyter
-      njpwerner.autodocstring
-  ];
   };
 }
