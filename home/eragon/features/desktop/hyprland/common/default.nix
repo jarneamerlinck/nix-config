adf52ac5e86f9d82b1bf709a4bdf17a15b8f8330
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-21 23:42:40 +0200

    template for hyprland

---
+++ home/eragon/features/desktop/hyprland/common/default.nix
@@ -0,0 +1,14 @@
+{ inputs, lib, pkgs, config, outputs, ... }:
+let
+  monitor = lib.head (lib.filter (m: m.primary) config.monitors);
+  i_modifier = "Mod4";
+in
+{
+  imports = [
+    ../../common
+    ./wayland-common.nix
+  ];
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
+}
