73451d3a4400f401b95844cb17c693812c7021e1
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-22 11:55:48 +0200

    moved greet-session from script to derivation

--- home/eragon/features/desktop/common/wayland-common.nix
+++ home/eragon/features/desktop/common/wayland-common.nix
@@ -1,11 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  shell_script = "hyprland";
+in
+{
   home = {
     packages = with pkgs; [
       wl-clipboard
-      (pkgs.writeShellScriptBin "greetd-session" ''
-        #!/bin/sh
-        exec Hyprland
-      '')
+      (pkgs.stdenv.mkDerivation {
+        name = "greetd-session";
+        phases = [ "installPhase" ];
+        installPhase = ''
+          mkdir -p $out/bin
+          echo '#!/usr/bin/env sh' > $out/bin/greetd-session
+          echo 'exec ${shell_script}' >> $out/bin/greetd-session
+          chmod +x $out/bin/greetd-session 
+        '';
+      })
     ];
   };
 }
