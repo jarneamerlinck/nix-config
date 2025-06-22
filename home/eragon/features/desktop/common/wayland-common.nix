c9bd23bb8926d4ef55ffaa8d9d62fd6280a280d3
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-22 11:28:25 +0200

    add greetd script

--- home/eragon/features/desktop/common/wayland-common.nix
+++ home/eragon/features/desktop/common/wayland-common.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }: {
   home = {
-    packages = with pkgs; [ wl-clipboard ];
+    packages = with pkgs; [
+      wl-clipboard
+      (pkgs.writeShellScriptBin "greetd-session" ''
+        #!/bin/sh
+        exec Hyprland  # or sway, or a custom script
+      '')
+    ];
   };
 }
