edb0a02fe3d150faa352aad686b566fcc233067b
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-22 15:17:01 +0200

    added config

--- home/eragon/features/desktop/hyprland/common/default.nix
+++ home/eragon/features/desktop/hyprland/common/default.nix
@@ -15,5 +15,14 @@ in
         "${toString monitor.name}, ${toString monitor.width}x${toString monitor.height}@${toString monitor.refreshRate}, auto, auto"
       ];
     };
+    extraConfig = ''
+      workspace = w[tv1], gapsout:0, gapsin:0
+      workspace = f[1], gapsout:0, gapsin:0
+      windowrule = bordersize 0, floating:0, onworkspace:w[tv1]
+      windowrule = rounding 0, floating:0, onworkspace:w[tv1]
+      windowrule = bordersize 0, floating:0, onworkspace:f[1]
+      windowrule = rounding 0, floating:0, onworkspace:f[1]
+      bind = SUPER, B, exec, firefox
+    '';
   };
 }
