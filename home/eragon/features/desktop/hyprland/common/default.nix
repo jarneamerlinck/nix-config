7fdaafc3c4ab14a45e68c55c43f6934c04cb169f
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-22 15:36:45 +0200

    set belgian keyboard

--- home/eragon/features/desktop/hyprland/common/default.nix
+++ home/eragon/features/desktop/hyprland/common/default.nix
@@ -14,15 +14,11 @@ in
       monitor = [
         "${toString monitor.name}, ${toString monitor.width}x${toString monitor.height}@${toString monitor.refreshRate}, auto, auto"
       ];
+      input =  {
+        kb_layout = "be";
+      };
     };
     extraConfig = ''
-      workspace = w[tv1], gapsout:0, gapsin:0
-      workspace = f[1], gapsout:0, gapsin:0
-      windowrule = bordersize 0, floating:0, onworkspace:w[tv1]
-      windowrule = rounding 0, floating:0, onworkspace:w[tv1]
-      windowrule = bordersize 0, floating:0, onworkspace:f[1]
-      windowrule = rounding 0, floating:0, onworkspace:f[1]
-      bind = SUPER, B, exec, firefox
     '';
   };
 }
