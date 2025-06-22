2512c4263552248b927d4669e04bf2a92d10231d
jarneamerlinck <jarneamerlinck@pm.me>
2025-06-22 16:47:44 +0200

    remove col =

--- home/eragon/features/desktop/hyprland/common/default.nix
+++ home/eragon/features/desktop/hyprland/common/default.nix
@@ -38,10 +38,8 @@ in
         gaps_in = 5;
         gaps_out = 20;
         border_size = 2;
-        col = {
-          active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-          inactive_border = "rgba(595959aa)";
-        };
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border"" = "rgba(595959aa)";
         resize_on_border = false;
         allow_tearing = false;
         layout = "dwindle";
