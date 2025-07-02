cc9e789bb6ccc61e712f009b5cf0406c7a1f3b8c
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-02 22:50:31 +0200

    added base home manager config for baruuk

---
+++ home/eragon/baruuk.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, inputs,  ... }:
+{
+  imports = [
+    ./common
+    ./features/desktop/sway/minimalistic
+  ];
+  wallpaper = pkgs.wallpapers.framework-12-grid;
+
+  colorScheme = inputs.nix-colors.colorSchemes.equilibrium-dark;
+
+  monitors = [{
+    name = "eDP-1";
+    width = 1920;
+    height = 1200;
+    workspace = "1";
+    primary = true;
+  }];
+
+}
+
