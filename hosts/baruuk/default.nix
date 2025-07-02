da5479911b48d11469145c3b4485b63c16970d16
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-02 22:44:36 +0200

    added baruuk host files

---
+++ hosts/baruuk/default.nix
@@ -0,0 +1,26 @@
+{
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.hardware.nixosModules.common-pc-ssd
+
+    ./hardware-configuration.nix
+    ../common/disks/boot_1d_btrfs.nix
+
+    ../common/base
+    ../common/users/eragon
+    ../common/disks/wd-decrypt.nix
+
+    ## Services items
+    ../common/services/unattended-upgrades.nix
+
+  ];
+
+  networking = {
+    hostName = "baruuk";
+    useDHCP = true;
+  };
+
+  system.stateVersion = "25.05";
+}
