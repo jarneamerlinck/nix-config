de37584b2363ab888a74c4ab8e9a655f3bd09457
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-03 21:32:07 +0200

    Revert "temp disable"
    This reverts commit 71874932949c3cb2ed574b2f1c6471e84ca22293.

--- home/eragon/baruuk.nix
+++ home/eragon/baruuk.nix
@@ -2,19 +2,19 @@
 {
   imports = [
     ./common
-    # ./features/desktop/sway/minimalistic
+    ./features/desktop/sway/minimalistic
   ];
-  # wallpaper = pkgs.wallpapers.framework-12-grid;
-  #
-  # colorScheme = inputs.nix-colors.colorSchemes.equilibrium-dark;
-  #
-  # monitors = [{
-  #   name = "eDP-1";
-  #   width = 1920;
-  #   height = 1200;
-  #   workspace = "1";
-  #   primary = true;
-  # }];
+  wallpaper = pkgs.wallpapers.framework-12-grid;
+
+  colorScheme = inputs.nix-colors.colorSchemes.equilibrium-dark;
+
+  monitors = [{
+    name = "eDP-1";
+    width = 1920;
+    height = 1200;
+    workspace = "1";
+    primary = true;
+  }];
 
 }
 
