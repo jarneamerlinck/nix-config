b5ba037fd58c1266ef15eb6ae787b6c4651a494a
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-04 20:43:15 +0200

    added wifi network for MEP

--- hosts/common/desktop/wireless.nix
+++ hosts/common/desktop/wireless.nix
@@ -16,24 +16,56 @@
         config.sops.secrets."wireless/env".path
       ];
       profiles = {
+        home-wifi = {
+          connection = {
+            id = "home-wifi";
+            permissions = "";
+            type = "wifi";
+          };
+          ipv4 = { method = "auto";};
+          wifi = {
+            mode = "infrastructure";
+            ssid = "$HOME_WIFI_SSID";
+          };
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+            psk = "$HOME_WIFI_PSK";
+          };
+        };
         home-dad = {
           connection = {
             id = "home-dad";
             permissions = "";
             type = "wifi";
           };
-          ipv4 = {
-            method = "auto";
-          };
+          ipv4 = { method = "auto";};
           wifi = {
             mode = "infrastructure";
             ssid = "$HOME_DAD_SSID";
           };
           wifi-security = {
             key-mgmt = "wpa-psk";
-            psk = "$HOME_DAD_PASS";
+            psk = "$HOME_DAD_PSK";
           };
         };
+
+        home-mep = {
+          connection = {
+            id = "home-mep";
+            permissions = "";
+            type = "wifi";
+          };
+          ipv4 = { method = "auto";};
+          wifi = {
+            mode = "infrastructure";
+            ssid = "$HOME_MEP_SSID";
+          };
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+            psk = "$HOME_MEP_PSK";
+          };
+        };
+
       };
 
     };
