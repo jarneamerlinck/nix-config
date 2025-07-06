b2b8f1dc3955dd013d2e8ebb450cfe870e1b2cf6
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-06 19:23:08 +0200

    tried harware keys for brightness and sound

--- home/eragon/features/desktop/sway/minimalistic/default.nix
+++ home/eragon/features/desktop/sway/minimalistic/default.nix
@@ -65,6 +65,9 @@ in
 
         "XF86MonBrightnessDown" = "exec light -U 10";
         "XF86MonBrightnessUp" =  "exec light -A 10";
+        "XF86AudioRaiseVolume" =  "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'";
+        "XF86AudioLowerVolume" =  "exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'";
+        "XF86AudioMute" =  "exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
       };
     };
     extraConfig = ''
