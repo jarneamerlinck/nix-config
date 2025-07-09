f31f7f6d935d84ceb1625181465012bdb7debaac
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-09 22:25:30 +0200

    sway swaylock package path

--- home/eragon/features/desktop/sway/minimalistic/default.nix
+++ home/eragon/features/desktop/sway/minimalistic/default.nix
@@ -46,7 +46,7 @@ in
         "${cfg.modifier}+Shift+Down" = "move down";
         "${cfg.modifier}+Shift+Up" = "move up";
         "${cfg.modifier}+Shift+Right" = "move right";
-        "${cfg.modifier}+Escape" = "exec ${pkgs.swaylock-effects}/bin/swaylock";
+        "${cfg.modifier}+Escape" = "exec ${config.programs.swaylock.package}/bin/swaylock";
         # "${cfg.modifier}+Shift+Escape" = "exec ${pkgs.swaylock-effects}/bin/swaylock -C /home/${config.home.username}/.config/swaylock/config";
         "${cfg.modifier}+Shift+l" = "exec swaymsg exit";
         "${cfg.modifier}+Shift+r" = "exec swaymsg reload";
