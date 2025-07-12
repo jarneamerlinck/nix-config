cafabbdcb0a4039a864cae6afcd6cbcb6670a9a1
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-12 19:37:49 +0200

    moved wallpaper list to host nix file

--- home/eragon/features/desktop/common/swww.nix
+++ home/eragon/features/desktop/common/swww.nix
@@ -8,17 +8,6 @@
   wallpaperListFile = "${config.xdg.configHome}/wallpaper_list.txt";
 in
 {
-  wallpaper-list =  with pkgs.wallpapers; [
-    framework-12-grid-16-10
-    linkin-park-logo-4k-qu-16-10
-    linkin-park-burning-in-the-skies-r1
-    sunset-over-the-peaks-jx-16-10
-    flare-lines-vr-16-10
-    star-trails-5k-i0-16-10
-    red-dragon-sky-16-10
-    falling-numbers-16-10-gif
-    rotating-waves-gif
-  ];
   home.file."${wallpaperListFile}".text = lib.concatStringsSep "\n" (config.wallpaper-list);
 
 
