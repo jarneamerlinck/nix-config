f1c89fd9db99b0f5708ef21a7a2723569323328a
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-19 20:55:21 +0200

    clean up applications of vm1

--- home/eragon/vm1.nix
+++ home/eragon/vm1.nix
@@ -1,35 +1,28 @@
-{ lib, pkgs, inputs,  ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   imports = [
     ./common
     ./features/services/git-config.nix
     ./features/cli/debugging.nix
     ./features/desktop/sway/minimalistic
-    ./features/music
-    ./features/cyber
-    ./features/cyber/pentesting
-    ./features/cyber/exploration/wireshark.nix
-
-    ./features/desktop/applications
-    ./features/desktop/applications/media_player.nix
-    ./features/desktop/applications/remote_desktop.nix
-    ./features/desktop/applications/discord.nix
-    ./features/desktop/applications/vscode.nix
-    ./features/desktop/applications/office.nix
-    ./features/desktop/applications/mouse.nix
-    ./features/desktop/applications/image_editing.nix
-    ./features/desktop/applications/video_editing.nix
   ];
   wallpaper = pkgs.wallpapers.nixos-logo;
 
   colorScheme = inputs.nix-colors.colorSchemes.bright;
 
-  monitors = [{
-    name = "eDP-1";
-    width = 1600;
-    height = 900;
-    workspace = "1";
-    primary = true;
-  }];
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1600;
+      height = 900;
+      workspace = "1";
+      primary = true;
+    }
+  ];
 
 }
