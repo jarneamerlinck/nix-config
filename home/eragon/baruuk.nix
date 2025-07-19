4475227b6723d0de483362fe93ba13ce94518eec
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-19 17:51:31 +0200

    remove wallpapers

--- home/eragon/baruuk.nix
+++ home/eragon/baruuk.nix
@@ -16,14 +16,8 @@
     ./features/desktop/applications/excalidraw_desktop_icon.nix
     ./features/desktop/common/swww.nix
   ];
-  # wallpaper = pkgs.wallpapers.framework-12-grid;
   wallpaper-list =  with pkgs.wallpapers; [
     framework-12-grid-16-10
-    linkin-park-logo-4k-qu-16-10
-    linkin-park-burning-in-the-skies-r1
-    rotating-waves-gif
-    meteor-shower-apocalypse-gif
-    giant-sun-falls-gif
   ];
 
   colorScheme = inputs.nix-colors.colorSchemes.equilibrium-dark;
