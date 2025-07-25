97894c5b71a6742df5886369fe7ecd98150945b4
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-25 23:20:59 +0200

    vestkop also from stylix

--- home/eragon/features/applications/base/discord.nix
+++ home/eragon/features/applications/base/discord.nix
@@ -1,72 +1,7 @@
 {
-  config,
-  pkgs,
   ...
 }:
 {
-  home.packages = with pkgs; [ vesktop ];
+  programs.vesktop.enable = true;
 
-  xdg.configFile."vesktop/themes/midnight.css".text =
-    # css
-    ''
-      @import url('https://refact0r.github.io/midnight-discord/midnight.css');
-
-        /* customize things here */
-        :root {
-            /* font, change to 'gg sans' for default discord font */
-            --font: 'cosmic_text';
-
-            /* top left corner text */
-            --corner-text: 'Nord';
-
-            /* color of status indicators and window controls */
-
-            /* accent colors */
-            --accent-1: hsl(179, 25%, 65%); /* links */
-            --accent-2: hsl(193, 43%, 67%); /* general unread/mention elements */
-            --accent-3: hsl(193, 43%, 67%); /* accent buttons */
-            --accent-4: hsl(193, 37%, 60%); /* accent buttons when hovered */
-            --accent-5: hsl(193, 31%, 53%); /* accent buttons when clicked */
-            --mention: hsla(193, 43%, 51%, 0.1); /* mentions & mention messages */
-            --mention-hover: hsla(193, 43%, 51%, 0.05); /* mentions & mention messages when hovered */
-
-            /* text colors */
-
-            /* background and dark colors */
-            --hover: hsla(220, 17%, 32%, 0.3); /* buttons when hovered */
-            --active: hsla(220, 17%, 32%, 0.5); /* channels and buttons when clicked or selected */
-            --message-hover: hsla(0, 0%, 19%, 1); /* messages when hovered */
-
-            /* amount of spacing and padding */
-            --spacing: 25px;
-
-            /* animations */
-            /* ALL ANIMATIONS CAN BE DISABLED WITH REDUCED MOTION IN DISCORD SETTINGS */
-            --list-item-transition: 0.2s ease; /* channels/members/settings hover transition */
-            --unread-bar-transition: 0.2s ease; /* unread bar moving into view transition */
-            --moon-spin-transition: 0.4s ease; /* moon icon spin */
-            --icon-spin-transition: 1s ease; /* round icon button spin (settings, emoji, etc.) */
-
-            /* corner roundness (border-radius) */
-            --roundness-xl: 8px; /* roundness of big panel outer corners */
-            --roundness-l: 8px; /* popout panels */
-            --roundness-m: 8px; /* smaller panels, images, embeds */
-            --roundness-s: 8px; /* members, settings inputs */
-            --roundness-xs: 8px; /* channels, buttons */
-            --roundness-xxs: 8px; /* searchbar, small elements */
-
-            /* direct messages moon icon */
-            /* change to block to show, none to hide */
-            --discord-icon: none; /* discord icon */
-            --moon-icon: block; /* moon icon */
-            # --moon-icon-url: url('https://upload.wikimedia.org/wikipedia/commons/3/35/Nix_Snowflake_Logo.svg'); /* custom icon url */
-            --moon-icon-size: auto;
-
-            /* filter uncolorable elements to fit theme */
-            /* (just set to none, they're too much work to configure) */
-            --login-bg-filter: none; /* login background artwork */
-            --green-to-accent-3-filter: none; /* add friend page explore icon */
-            --blurple-to-accent-3-filter: none; /* add friend page school icon */
-        }
-    '';
 }
