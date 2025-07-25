2f66a9d0188c5c789d0b491c44316e47336dd69e
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-25 23:33:22 +0200

    add extra settings for vesktop

--- home/eragon/features/applications/base/discord.nix
+++ home/eragon/features/applications/base/discord.nix
@@ -2,6 +2,24 @@
   ...
 }:
 {
-  programs.vesktop.enable = true;
+  programs.vesktop = {
+
+    enable = true;
+    settings = {
+      appBadge = false;
+      arRPC = true;
+      checkUpdates = false;
+      customTitleBar = false;
+      disableMinSize = true;
+      minimizeToTray = false;
+      tray = false;
+      splashBackground = "#000000";
+      splashColor = "#ffffff";
+      splashTheming = true;
+      staticTitle = true;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+    };
+  };
 
 }
