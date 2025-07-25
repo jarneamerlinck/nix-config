34229ddbb6ac45c024c220d09d86208d32ef8182
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-25 21:12:43 +0200

    disable GTK

--- hosts/features/desktop/gtk.nix
+++ hosts/features/desktop/gtk.nix
@@ -2,12 +2,12 @@
   ...
 }:
 {
-  environment.etc."xdg/gtk-2.0/gtkrc".text = ''
-    gtk-theme-name = "nix-color"
-  '';
-
-  environment.etc."xdg/gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name = nix-color
-  '';
+  # environment.etc."xdg/gtk-2.0/gtkrc".text = ''
+  #   gtk-theme-name = "nix-color"
+  # '';
+  #
+  # environment.etc."xdg/gtk-3.0/settings.ini".text = ''
+  #   [Settings]
+  #   gtk-theme-name = nix-color
+  # '';
 }
