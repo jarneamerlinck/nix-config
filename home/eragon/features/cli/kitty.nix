e7f01d46508f6ddc58745f16d2667f6c9359f45f
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-25 20:58:54 +0200

    removed colors from kitty

--- home/eragon/features/cli/kitty.nix
+++ home/eragon/features/cli/kitty.nix
@@ -1,30 +1,13 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   rmHash = lib.removePrefix "#";
-  inherit (config.colorscheme) palette;
   kittyColors = ''
     # Colors generated from nix-colors
-    foreground #${rmHash palette.base05}
-    background #${rmHash palette.base00}
-    cursor #${rmHash palette.base05}
-
-    color0  #${rmHash palette.base00}
-    color1  #${rmHash palette.base08}
-    color2  #${rmHash palette.base0B}
-    color3  #${rmHash palette.base0A}
-    color4  #${rmHash palette.base0D}
-    color5  #${rmHash palette.base0E}
-    color6  #${rmHash palette.base0C}
-    color7  #${rmHash palette.base05}
-
-    color8  #${rmHash palette.base03}
-    color9  #${rmHash palette.base08}
-    color10 #${rmHash palette.base0B}
-    color11 #${rmHash palette.base0A}
-    color12 #${rmHash palette.base0D}
-    color13 #${rmHash palette.base0E}
-    color14 #${rmHash palette.base0C}
-    color15 #${rmHash palette.base07}
   '';
 in
 {
@@ -102,7 +85,7 @@ in
       initial_window_width = 2500;
       initial_window_height = 1380;
       macos_titlebar_color = "background";
-      };
-      extraConfig = kittyColors;
+    };
+    extraConfig = kittyColors;
   };
 }
