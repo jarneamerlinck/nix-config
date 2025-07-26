3a9447a70ceeaad97bf844ed5e72ec904411233c
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-26 18:53:55 +0200

    retried stylix

--- home/eragon/features/desktop/sway/minimalistic/swaylock.nix
+++ home/eragon/features/desktop/sway/minimalistic/swaylock.nix
@@ -7,41 +7,9 @@ let
   palette = config.lib.stylix.colors;
 in
 {
-  stylix.targets.swaylock.enable = false;
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
-    settings = {
-      clock = true;
-      show-failed-attempts = true;
-      indicator = true;
-      indicator-radius = 200;
-      indicator-thickness = 20;
-      line-uses-ring = false;
-      grace = 0;
-      grace-no-mouse = true;
-      grace-no-touch = true;
-      datestr = "%d.%m";
-      fade-in = "0.1";
-      ignore-empty-password = true;
-
-      # Own settings
-      color = "${palette.base00}";
-      inside-color = "${palette.base00}";
-      font = "${config.fontProfiles.monospace.family}";
-      text-color = "${palette.base05}";
-      text-caps-lock-color = "${palette.base07}";
-      line-color = "${palette.base01}";
-
-      ring-color = "${palette.base06}";
-      key-h1-color = "${palette.base0A}";
-
-      ring-clear-color = "${palette.base00}";
-
-      ring-wrong-color = "${palette.base09}";
-      inside-wrong-color = "${palette.base09}";
-
-    };
   };
 
   services.swayidle = {
@@ -59,4 +27,5 @@ in
       }
     ];
   };
+  stylix.targets.swaylock.enable = true;
 }
