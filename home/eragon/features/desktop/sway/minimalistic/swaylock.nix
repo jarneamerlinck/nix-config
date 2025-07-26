289f97d4a49b5375f316db7f34acca6c69ab1d27
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-26 13:12:53 +0200

    swaylock back from swaylock-effects

--- home/eragon/features/desktop/sway/minimalistic/swaylock.nix
+++ home/eragon/features/desktop/sway/minimalistic/swaylock.nix
@@ -6,22 +6,8 @@
 {
   programs.swaylock = {
     enable = true;
-    package = pkgs.swaylock-effects;
-    settings = {
-      clock = true;
-      show-failed-attempts = true;
-      indicator = true;
-      indicator-radius = 200;
-      indicator-thickness = 20;
-      line-uses-ring = false;
-      grace = 0;
-      grace-no-mouse = true;
-      grace-no-touch = true;
-      datestr = "%d.%m";
-      fade-in = "0.1";
-      ignore-empty-password = true;
+    # package = pkgs.swaylock-effects;
 
-    };
   };
 
   services.swayidle = {
