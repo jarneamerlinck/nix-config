b71dd3e7b837d96bbcb2960fa7ef173869815ce0
jarneamerlinck <jarneamerlinck@pm.me>
2025-08-16 16:51:47 +0200

    swap cache key

--- hosts/features/services/nix_cache_vms.nix
+++ hosts/features/services/nix_cache_vms.nix
@@ -2,12 +2,12 @@
 {
   nix.settings = {
     substituters = [
-      "https://nix_cache.ko0.net/"
+      "https://nix.cache.ko0.net/"
       "https://cache.nixos.org/"
     ];
 
     trusted-public-keys = [
-      "nix_cache.ko0.net:ggf19YIFJVf0lyfJcP41xxooRehNnraLFtU5tX8MA/Y="
+      "nix.cache.ko0.net:GtHtHec2fVedkzmQyaBu9/Ug7qWGGm4WA3joz3beTdc="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
 
