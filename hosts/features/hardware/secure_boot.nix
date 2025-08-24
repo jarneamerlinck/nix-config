dd608a41990a9219d90aa44c0f85eb49cc3cd43e
jarneamerlinck <jarneamerlinck@pm.me>
2025-08-24 14:33:07 +0200

    swap systemd-boot out for lanzaboote

--- hosts/features/hardware/secure_boot.nix
+++ hosts/features/hardware/secure_boot.nix
@@ -1,8 +1,20 @@
 {
   config,
   inputs,
+  lib,
   ...
 }:
 {
   imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
+
+  boot = {
+    initrd.systemd.enable = true;
+
+    loader.systemd-boot.enable = lib.mkForce false;
+
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/etc/secureboot";
+    };
+  };
 }
