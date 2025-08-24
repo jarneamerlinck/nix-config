1b96411ef284ad17cf3100a0f11a62874e79fa34
jarneamerlinck <jarneamerlinck@pm.me>
2025-08-24 11:35:31 +0200

    revert to swap with btrfs

--- hosts/features/hardware/laptop.nix
+++ hosts/features/hardware/laptop.nix
@@ -13,12 +13,12 @@
     lidSwitchExternalPower = "sleep";
     lidSwitchDocked = "ignore";
   };
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 10240;
-    }
-  ];
+  # swapDevices = [
+  #   {
+  #     device = "/var/lib/swapfile";
+  #     size = 10240;
+  #   }
+  # ];
   boot.resumeDevice = "/dev/vda3";
   boot.kernelParams = [ "resume_offset=0" ];
   powerManagement.enable = true;
