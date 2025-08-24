162900099d7646844bafa44ab82d6ed97eadf3a5
jarneamerlinck <jarneamerlinck@pm.me>
2025-08-24 11:17:39 +0200

    added resume option

--- hosts/features/hardware/laptop.nix
+++ hosts/features/hardware/laptop.nix
@@ -20,6 +20,7 @@
   #   }
   # ];
   boot.resumeDevice = "/dev/vda3";
+  boot.kernelParams = [ "resume_offset=0" ];
   powerManagement.enable = true;
   systemd.sleep.extraConfig = "HibernateDelaySec=1h";
 }
