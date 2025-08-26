99ad6cc5939b396503b62225120d126098b6f8cf
jarneamerlinck <jarneamerlinck@pm.me>
2025-08-26 20:25:22 +0200

    update fresh rss

--- hosts/features/virtualization/docker/services/rss_feed.nix
+++ hosts/features/virtualization/docker/services/rss_feed.nix
@@ -4,7 +4,7 @@
 {
   # Containers
   virtualisation.oci-containers.containers."freshrss" = {
-    image = "lscr.io/linuxserver/freshrss:1.24.3-ls244";
+    image = "lscr.io/linuxserver/freshrss:1.27.0";
     environment = {
       "PGID" = "100";
       "PUID" = "1442";
