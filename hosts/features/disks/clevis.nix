836e8d50de0dd16dffcbee220090b9ec1f6229f0
jarneamerlinck <jarneamerlinck@pm.me>
2025-09-19 09:47:21 +0200

    Revert "tries yubikey"
    This reverts commit e49101409966621238052e969c9be2fafccfb298.

--- hosts/features/disks/clevis.nix
+++ hosts/features/disks/clevis.nix
@@ -4,8 +4,6 @@
   # https://support.yubico.com/hc/en-us/articles/360016649039-Installing-Yubico-Software-on-Linux
   # https://www.privacyguides.org/articles/2025/03/06/yubikey-reset-and-backup/#step-6-reset-your-yubikeys-openpgp-and-yubihsm-auth-applicationsA
   # https://github.com/NixOS/nixpkgs/issues/389750
-  # https://nixos.wiki/wiki/Yubikey_based_Full_Disk_Encryption_(FDE)_on_NixOs
-  # https://joinemm.dev/blog/yubikey-nixos-guide
   # https://haseebmajid.dev/posts/2024-07-30-how-i-setup-btrfs-and-luks-on-nixos-using-disko/
 
   # TODO: allow yubi key to decrypt: https://haseebmajid.dev/posts/2024-07-30-how-i-setup-btrfs-and-luks-on-nixos-using-disko/
