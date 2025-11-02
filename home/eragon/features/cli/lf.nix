2a8100ebe1867ea22d42cba355dc1489f45420dc
jarneamerlinck <jarneamerlinck@pm.me>
2025-11-02 21:29:35 +0100

    Revert "try to allow ssh"
    This reverts commit 2b5523d34db096a52add6dcb5da1c8b758739087.

--- home/eragon/features/cli/lf.nix
+++ home/eragon/features/cli/lf.nix
@@ -32,7 +32,6 @@
 
       # ...
     };
-
     extraConfig =
       let
         previewer = pkgs.writeShellScriptBin "pv.sh" ''
@@ -42,47 +41,15 @@
           x=$4
           y=$5
 
-          # Small scaling tweak for lf
-          w=$(( w * 99 / 100 ))
-          h=$(( h * 99 / 100 ))
-          y=$(( y + 1 ))
-
-          # Determine mime type
           if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-            # Detect remote path like user@host:/path/to/image.png
-            if [[ "$file" =~ ^([^:]+):(/.*) ]]; then
-              host="''${BASH_REMATCH[1]}"
-              remote_path="''${BASH_REMATCH[2]}"
-
-              # Stream remote image data to local kitty (works over plain ssh)
-              ssh "''${host}" "cat \"''${remote_path}\"" | \
-                ${pkgs.kitty}/bin/kitty +kitten icat \
-                  --transfer-mode buffer \
-                  --stdin yes \
-                  --place "''${w}x''${h}@''${x}x''${y}" - < /dev/null > /dev/tty
+              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
               exit 1
-            fi
-
-            # Local file path: use kitty directly
-            ${pkgs.kitty}/bin/kitty +kitten icat \
-              --silent \
-              --stdin no \
-              --transfer-mode file \
-              --place "''${w}x''${h}@''${x}x''${y}" "$file" \
-              < /dev/null > /dev/tty
-            exit 1
           fi
 
-          # Fallback to pistol for non-image files
           ${pkgs.pistol}/bin/pistol "$file"
         '';
-
         cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-          ${pkgs.kitty}/bin/kitty +kitten icat \
-            --clear \
-            --stdin no \
-            --silent \
-            --transfer-mode file < /dev/null > /dev/tty
+          ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
         '';
       in
       ''
