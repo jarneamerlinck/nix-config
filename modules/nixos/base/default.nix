7ea8f6e73ec8a62142bccb0a7830c18c071a1eb6
jarneamerlinck <jarneamerlinck@pm.me>
2025-11-20 18:35:30 +0100

    Added test for module import

---
+++ modules/nixos/base/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+
+  options = {
+
+    base = {
+      enable = lib.mkEnableOption "enables base module";
+      zsh.enable = lib.mkDefault true;
+      sops.enable = lib.mkDefault true;
+      openssh.enable = lib.mkDefault true;
+      neovim.enable = lib.mkDefault true;
+      nix.enable = lib.mkDefault true;
+      security.enable = lib.mkDefault true;
+    };
+  };
+
+}
