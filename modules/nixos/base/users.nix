77a226b562819fd971db21ed8b621343ec006431
jarneamerlinck <jarneamerlinck@pm.me>
2025-11-22 21:03:30 +0100

    added home configs

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -160,7 +160,7 @@ in
     sops.secrets = perUserSecrets;
 
     # Home configuration creation
-    # joinedLib.homeConfigurations = homeConfs;
+    joinedLib.homeConfigurations = homeConfs;
 
     # Activate home manager rebuild
     # home-manager.users = builtins.listToAttrs (
