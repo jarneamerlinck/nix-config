1bd1667361dfff05e2c90f5dc7baec1ba0d46837
jarneamerlinck <jarneamerlinck@pm.me>
2025-11-22 20:50:31 +0100

    disable home-manager for testing

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -169,12 +169,12 @@ in
     # joinedLib.homeConfigurations = homeConfs;
 
     # Activate home manager rebuild
-    home-manager.users = builtins.listToAttrs (
-      map (username: {
-        name = username;
-        value = import ../../../home/${username}/${config.networking.hostName};
-
-      }) users
-    );
+    # home-manager.users = builtins.listToAttrs (
+    #   map (username: {
+    #     name = username;
+    #     value = import ../../../home/${username}/${config.networking.hostName};
+    #
+    #   }) users
+    # );
   };
 }
