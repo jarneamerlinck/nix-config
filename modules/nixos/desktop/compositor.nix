b89f1086336fb134a5d3d3f7cc5596835e40d126
jarneamerlinck <jarneamerlinck@pm.me>
2025-12-07 16:46:17 +0100

    moved shared code

--- modules/nixos/desktop/compositor.nix
+++ modules/nixos/desktop/compositor.nix
@@ -34,14 +34,17 @@
   config = lib.mkIf config.desktop.compositor.enable (
     lib.mkMerge [
 
-      (lib.mkIf (config.desktop.compositor.enable) {
+      # (lib.mkIf (config.desktop.compositor.enable) {
+      #
+      #   systemd.services."getty@tty1".enable = false;
+      #   systemd.services."autovt@tty1".enable = false;
+      #   programs.dconf.enable = true;
+      # })
+      (lib.mkIf (config.desktop.compositor.type == "x11") {
 
         systemd.services."getty@tty1".enable = false;
         systemd.services."autovt@tty1".enable = false;
         programs.dconf.enable = true;
-      })
-      (lib.mkIf (config.desktop.compositor.type == "x11") {
-
         environment.systemPackages = with pkgs; [ xdg-desktop-portal ];
         services.xserver = {
           enable = true;
@@ -52,6 +55,9 @@
 
       (lib.mkIf (config.desktop.compositor.type == "wayland") {
 
+        systemd.services."getty@tty1".enable = false;
+        systemd.services."autovt@tty1".enable = false;
+        programs.dconf.enable = true;
         environment.sessionVariables.NIXOS_OZONE_WL = "1";
         security.polkit.enable = true;
         hardware.graphics.enable = true; # Only enable inside VM
