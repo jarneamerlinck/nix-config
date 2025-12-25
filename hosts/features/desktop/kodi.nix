2fbd29989b923cd0403a5d752d5a50f08c76c7bf
jarneamerlinck <jarneamerlinck@pm.me>
2025-12-25 14:18:47 +0100

    use wayland for kodi

--- hosts/features/desktop/kodi.nix
+++ hosts/features/desktop/kodi.nix
@@ -9,7 +9,7 @@
     ))
   ];
 
-  services.xserver.enable = true;
+  services.xserver.enable = false;
   services.xserver.desktopManager.kodi.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kodi";
@@ -17,7 +17,11 @@
   services.xserver.displayManager.lightdm.greeter.enable = false;
 
   # Define a user account
+
   users.extraUsers.kodi.isNormalUser = true;
+  services.cage.user = "kodi";
+  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+  services.cage.enable = true;
 
   networking.firewall = {
     allowedTCPPorts = [ 8080 ];
