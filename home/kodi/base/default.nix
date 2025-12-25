82acdeac16566e78ca8253f82fb8282a1cccac6a
jarneamerlinck <jarneamerlinck@pm.me>
2025-12-25 15:31:28 +0100

    added start of home manager for kodi

---
+++ home/kodi/base/default.nix
@@ -0,0 +1,62 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  config,
+  outputs,
+  ...
+}:
+{
+  imports = [
+    # inputs.impermanence.nixosModules.home-manager.impermanence
+    inputs.stylix.homeModules.stylix
+    inputs.nur.modules.homeManager.default
+    inputs.sops-nix.homeManagerModule
+  ]
+  ++ (builtins.attrValues outputs.homeManagerModules);
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      warn-dirty = false;
+    };
+  };
+
+  systemd.user.startServices = "sd-switch";
+
+  programs = {
+    home-manager.enable = true;
+    git.enable = true;
+  };
+
+  home = {
+    username = lib.mkDefault "kodi";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "25.11";
+    sessionPath = [ "$HOME/.local/bin" ];
+    # sessionVariables = {
+    #   FLAKE = "$HOME/nix-config";
+    #   NH_FLAKE = "$HOME/nix-config";
+    # };
+  };
+
+  # Set default wallpaper and colorscheme
+  # Available color schemes can be found at
+  stylix.enable = true;
+  stylix.autoEnable = true;
+
+  stylix.image = lib.mkDefault "${pkgs.wallpapers.sci-fi-holographic-abstract}";
+  stylix.polarity = lib.mkDefault "dark";
+
+}
---
+++ home/kodi/vm1/default.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ../base
+  ];
+  # wallpaper = pkgs.wallpapers.nixos-logo;
+  #
+  # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1600;
+      height = 900;
+      workspace = "1";
+      primary = true;
+    }
+  ];
+
+}
