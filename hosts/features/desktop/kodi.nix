278cacdd27b5f877b1a7c9ee0375effda133b809
jarneamerlinck <jarneamerlinck@pm.me>
2025-12-25 14:29:57 +0100

    yet antoher try

--- hosts/features/desktop/kodi.nix
+++ hosts/features/desktop/kodi.nix
@@ -1,31 +1,29 @@
 { pkgs, ... }:
+let
+  kodi-with-addons = pkgs.kodi-wayland.withPackages (
+    kodiPkgs: with kodiPkgs; [
+      inputstream-adaptive
+      jellyfin
+      bluetooth-manager
+    ]
+  );
+in
 {
 
-  environment.systemPackages = [
-    (pkgs.kodi.withPackages (
-      kodiPkgs: with kodiPkgs; [
-        jellyfin
-      ]
-    ))
-  ];
-
-  # services.xserver.enable = false;
-  # services.xserver.desktopManager.kodi.enable = false;
-  # services.displayManager.autoLogin.enable = true;
-  # services.displayManager.autoLogin.user = "kodi";
-
-  # services.xserver.displayManager.lightdm.greeter.enable = false;
-
   # Define a user account
-
-  users.extraUsers.kodi.isNormalUser = true;
   services.cage.user = "kodi";
-  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+  services.cage.extraArguments = [
+    "-m"
+    "last"
+  ];
+  services.cage.program = "${kodi-with-addons}/bin/kodi-standalone";
   services.cage.enable = true;
-
-  networking.firewall = {
-    allowedTCPPorts = [ 8080 ];
-    allowedUDPPorts = [ 8080 ];
+  users.users.kodi = {
+    isNormalUser = true;
+    extraGroups = [
+      "uucp"
+      "audio"
+      "input"
+    ];
   };
-
 }
