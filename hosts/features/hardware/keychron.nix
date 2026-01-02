a72d156da40b5a1c0b2340a705e0cc6076c6207a
jarneamerlinck <jarneamerlinck@pm.me>
2026-01-03 00:49:43 +0100

    set aliases for keychron

--- hosts/features/hardware/keychron.nix
+++ hosts/features/hardware/keychron.nix
@@ -2,4 +2,13 @@
   services.udev.extraRules = ''
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0811", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
   '';
+
+  programs.bash.shellAliases = {
+    keychron = "nix shell nixpkgs#google-chrome --impure -c google-chrome-stable https://launcher.keychron.com";
+  };
+
+  programs.zsh.shellAliases = {
+    keychron = "nix shell nixpkgs#google-chrome --impure -c google-chrome-stable https://launcher.keychron.com";
+  };
+
 }
