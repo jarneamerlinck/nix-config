138219be97deede85cdd41727852716ccad577aa
jarneamerlinck <jarneamerlinck@pm.me>
2026-01-24 22:00:18 +0100

    added feature cleaning

--- home/eragon/features/desktop/sway/common/default.nix
+++ home/eragon/features/desktop/sway/common/default.nix
@@ -1,15 +1,12 @@
 {
-  inputs,
   lib,
-  pkgs,
   config,
-  outputs,
   ...
 }:
-let
-  monitor = lib.head (lib.filter (m: m.primary) config.monitors);
-  i_modifier = "Mod4";
-in
+# let
+#   monitor = lib.head (lib.filter (m: m.primary) config.monitors);
+#   i_modifier = "Mod4";
+# in
 {
   imports = [
     ../../common
