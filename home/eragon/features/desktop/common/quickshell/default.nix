996881c9258605336fc84ae03ac44ec87122a2ff
jarneamerlinck <jarneamerlinck@pm.me>
2026-02-14 09:47:08 +0100

    update quick shell

--- home/eragon/features/desktop/common/quickshell/default.nix
+++ home/eragon/features/desktop/common/quickshell/default.nix
@@ -1,7 +1,44 @@
-{ pkgs, ... }:
+{ config, ... }:
+let
+  fontSize = toString config.stylix.fonts.sizes.desktop;
+in
 {
   programs.quickshell = {
     enable = true;
     systemd.enable = true;
   };
+
+  xdg.configFile."quickshell/shell.qml".text = with config.lib.stylix.colors.withHashtag; ''
+    import Quickshell
+    # import "file:${config.home.homeDirectory}/.config/quickshell/components"
+
+    Scope {
+      id: root
+
+      property var theme: ({
+        base00: "${base00}",
+        base01: "${base01}",
+        base03: "${base03}",
+        base05: "${base05}",
+        base0C: "${base0C}",
+        base08: "${base08}",
+
+        monospaceFont: ${builtins.toJSON config.stylix.fonts.monospace.name},
+        emojiFont: ${builtins.toJSON config.stylix.fonts.emoji.name},
+        fontSize: ${fontSize},
+
+        barHeight: 40,
+        radius: 5,
+
+        widgetBg: "${base01}",
+        widgetBorder: "${base03}",
+        fg: "${base05}",
+        danger: "${base08}"
+      })
+
+      Bar {
+        theme: root.theme
+      }
+    }
+  '';
 }
