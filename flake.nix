ff168bfe9d0ffc5dc51a434a99ba384e7f49765c
jarneamerlinck <jarneamerlinck@pm.me>
2026-02-16 19:14:51 +0100

    commented on nix inputs

--- flake.nix
+++ flake.nix
@@ -4,44 +4,47 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    # System Base
+    hardware.url = "github:nixos/nixos-hardware";
+    # Disk formatting
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Passwords
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    compose2nix = {
-      url = "github:aksiksi/compose2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Hardware
-    hardware.url = "github:nixos/nixos-hardware";
+    # Theming
     stylix.url = "github:nix-community/stylix/release-25.11";
-
+    # Firefox extentions
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim-config.url = "github:jarneamerlinck/kickstart.nvim";
-    hexecute.url = "github:ThatOtherAndrew/Hexecute";
+    # Quickshell shell
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Nvim config
+    nixvim-config.url = "github:jarneamerlinck/kickstart.nvim";
+
+    # Optional
+    compose2nix = {
+      url = "github:aksiksi/compose2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Magic pen drawing
+    hexecute.url = "github:ThatOtherAndrew/Hexecute";
   };
 
   outputs =
