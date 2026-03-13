018d652d5062dd71409f2434811f8958b753b2da
jarneamerlinck <jarneamerlinck@pm.me>
2026-03-13 20:33:39 +0100

    upgrade openwebui

--- hosts/features/virtualization/docker/services/openwebui.nix
+++ hosts/features/virtualization/docker/services/openwebui.nix
@@ -21,7 +21,7 @@ in
   };
   # Containers
   virtualisation.oci-containers.containers."chat-openwebui" = {
-    image = "ghcr.io/open-webui/open-webui:0.8.8";
+    image = "ghcr.io/open-webui/open-webui:0.8.10";
     environmentFiles = [ "/run/secrets-for-users/openwebui/env" ];
 
     environment = {
@@ -54,7 +54,7 @@ in
   };
 
   virtualisation.oci-containers.containers."ollama" = {
-    image = "ollama/ollama:0.17.7";
+    image = "ollama/ollama:0.17.8";
     volumes = [ "/data/docker/chat/ollama/:/root/.ollama" ];
     environment = {
       "chat_HOST" = "0.0.0.0";
