74fbedafff3946011251bda96c6cdc065672df04
jarneamerlinck <jarneamerlinck@pm.me>
2026-05-05 20:47:00 +0200

    add docker container to try

--- hosts/vm1/specializations.nix
+++ hosts/vm1/specializations.nix
@@ -1,16 +1,17 @@
 { pkgs, config, ... }:
 {
   specialisation = {
-    default = {
-      inheritParentConfig = true;
-      configuration = {
-        system.nixos.tags = [ "default" ];
-      };
-    };
+    # default = {
+    #   inheritParentConfig = true;
+    #   configuration = {
+    #     system.nixos.tags = [ "default" ];
+    #   };
+    # };
 
     tablet = {
       inheritParentConfig = true;
       configuration = {
+        imports = [ ../features/virtualization/docker/services/drawio.nix ];
         system.nixos.tags = [ "tablet" ];
         services.cage.enable = true;
       };
