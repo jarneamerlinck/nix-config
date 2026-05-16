79ba88d4199d9ee4a5ae25d62e11433dfb0dd942
jarneamerlinck <jarneamerlinck@pm.me>
2026-05-16 16:08:21 +0200

    feat: firefox check for dark theme before applying dark reader

--- home/eragon/features/applications/base/firefox.nix
+++ home/eragon/features/applications/base/firefox.nix
@@ -130,60 +130,59 @@ in
     force = true;
     text = ''
       {
-          "schemeVersion": 2,
-          "enabled": true,
-          "fetchNews": true,
-          "theme": {
-      	"mode": 1,
-      	"brightness": 100,
-      	"contrast": 100,
-      	"grayscale": 0,
-      	"sepia": 0,
-      	"useFont": false,
-      	"fontFamily": "Open Sans",
-      	"textStroke": 0,
-      	"engine": "dynamicTheme",
-      	"stylesheet": "",
-      	"darkSchemeBackgroundColor": "${base00}",
-      	"darkSchemeTextColor": "${base05}",
-      	"lightSchemeBackgroundColor": "${base05}",
-      	"lightSchemeTextColor": "${base00}",
-      	"scrollbarColor": "auto",
-      	"selectionColor": "auto",
-      	"styleSystemControls": false,
-      	"lightColorScheme": "Default",
-      	"darkColorScheme": "Default",
-      	"immediateModify": false
-          },
-          "presets": [],
-          "customThemes": [],
-          "enabledByDefault": true,
-          "enabledFor": [],
-          "disabledFor": [],
-          "changeBrowserTheme": false,
-          "syncSettings": false,
-          "syncSitesFixes": true,
-          "automation": {
-      	"enabled": false,
-      	"mode": "",
-      	"behavior": "OnOff"
-          },
-          "time": {
-      	"activation": "18:00",
-      	"deactivation": "9:00"
-          },
-          "location": {
-      	"latitude": null,
-      	"longitude": null
-          },
-          "previewNewDesign": true,
-          "enableForPDF": true,
-          "enableForProtectedPages": true,
-          "enableContextMenus": false,
-          "detectDarkTheme": false,
-          "displayedNews": [
-      	"thanks-2023"
-          ]
+      "schemeVersion": 2,
+      "enabled": true,
+      "fetchNews": true,
+      "theme": {
+        "mode": 1,
+        "brightness": 100,
+        "contrast": 100,
+        "grayscale": 0,
+        "sepia": 0,
+        "useFont": false,
+        "fontFamily": "Open Sans",
+        "textStroke": 0,
+        "engine": "dynamicTheme",
+        "stylesheet": "",
+        "darkSchemeBackgroundColor": "${base00}",
+        "darkSchemeTextColor": "${base05}",
+        "lightSchemeBackgroundColor": "${base05}",
+        "lightSchemeTextColor": "${base00}",
+        "scrollbarColor": "auto",
+        "selectionColor": "auto",
+        "styleSystemControls": false,
+        "lightColorScheme": "Default",
+        "darkColorScheme": "Default",
+        "immediateModify": false
+      },
+      "presets": [],
+      "customThemes": [],
+      "enabledByDefault": true,
+      "enabledFor": [],
+      "disabledFor": [],
+      "changeBrowserTheme": false,
+      "syncSettings": false,
+      "syncSitesFixes": true,
+      "automation": {
+        "enabled": false,
+        "mode": "",
+        "behavior": "OnOff"
+      },
+      "time": {
+        "activation": "18:00",
+        "deactivation": "9:00"
+      },
+      "location": {
+        "latitude": null,
+        "longitude": null
+      },
+      "previewNewDesign": true,
+      "enableForPDF": true,
+      "enableForProtectedPages": true,
+      "enableContextMenus": false,
+      "detectDarkTheme": true,
+      "displayedNews": [
+      ]
       }
     '';
   };
