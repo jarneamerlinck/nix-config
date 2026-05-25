df4e657bb51ae2bfe2a05f6a7d7dddb209b999b8
jarneamerlinck <jarneamerlinck@pm.me>
2026-05-25 16:50:03 +0200

    update sops

--- hosts/features/services/hermes.nix
+++ hosts/features/services/hermes.nix
@@ -5,22 +5,22 @@
 }:
 {
 
-  # sops.secrets."hermes" = {
-  #   sopsFile = /../../${config.networking.hostName}/secrets.yml;
-  #   neededForUsers = false;
-  # };
+  sops.secrets."hermes/env" = {
+    sopsFile = ../../${config.networking.hostName}/secrets.yml;
+    neededForUsers = false;
+  };
   imports = [ inputs.hermes-agent.nixosModules.default ];
-  # services.hermes-agent = {
-  #   enable = true;
-  #   settings = {
-  #     model = {
-  #       default = "gpt-oss:20b";
-  #     };
-  #   };
-  #
-  #   environmentFiles = [ config.sops.secrets."hermes-env".path ];
-  #   addToSystemPackages = true;
-  #   container.enable = true;
-  #   container.hostUsers = [ "eragon" ];
-  # };
+  services.hermes-agent = {
+    enable = true;
+    settings = {
+      model = {
+        default = "gpt-oss:20b";
+      };
+    };
+
+    environmentFiles = [ config.sops.secrets."hermes/env".path ];
+    addToSystemPackages = true;
+    container.enable = true;
+    container.hostUsers = [ "eragon" ];
+  };
 }
