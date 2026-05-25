37b075d7f42ea4eadda50518d36079ce63f18f45
jarneamerlinck <jarneamerlinck@pm.me>
2026-05-25 16:11:51 +0200

    feat: init config for hermes

--- hosts/features/services/hermes.nix
+++ hosts/features/services/hermes.nix
@@ -4,6 +4,23 @@
   ...
 }:
 {
-  imports = [ inputs.hermes-agent.nixosModules.default ];
 
+  sops.secrets."hermes-env" = {
+    sopsFile = ../../../../${config.networking.hostName}/secrets.yml;
+    neededForUsers = true;
+  };
+  imports = [ inputs.hermes-agent.nixosModules.default ];
+  services.hermes-agent = {
+    enable = true;
+    settings = {
+      model = {
+        default = "gpt-oss:20b";
+      };
+    };
+
+    environmentFiles = [ config.sops.secrets."hermes-env".path ];
+    addToSystemPackages = true;
+    container.enable = true;
+    container.hostUsers = [ "eragon" ];
+  };
 }
