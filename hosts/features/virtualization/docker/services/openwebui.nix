f469d89231fc15e1bc1128b66eb458c21ad75f23
jarneamerlinck <jarneamerlinck@pm.me>
2026-05-25 18:15:58 +0200

    fix: roll back to fix api bug

--- hosts/features/virtualization/docker/services/openwebui.nix
+++ hosts/features/virtualization/docker/services/openwebui.nix
@@ -21,7 +21,7 @@ in
   };
   # Containers
   virtualisation.oci-containers.containers."chat-openwebui" = {
-    image = "ghcr.io/open-webui/open-webui:0.9.5";
+    image = "ghcr.io/open-webui/open-webui:0.9.4";
     environmentFiles = [ "/run/secrets-for-users/openwebui/env" ];
 
     environment = {
