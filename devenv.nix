2f685f5b38725ccd27edea61252b9c4798d164ce
jarneamerlinck <39766278+jarneamerlinck@users.noreply.github.com>
2026-05-31 12:01:35 +0200

    feat: added devenv (#137)

---
+++ devenv.nix
@@ -0,0 +1,68 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.sops
+    pkgs.pre-commit
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+  languages.nix = {
+    enable = true;
+
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
+  # enterShell = ''
+  #   hello         # Run scripts directly
+  #   git --version # Use packages
+  # '';
+
+  # https://devenv.sh/tasks/
+  tasks = {
+    "nix-update:flake" = {
+      exec = "nix flake update";
+    };
+    "nix-update:devenv" = {
+      exec = "devenv update";
+      # after = [ "nix:flake-update" ];
+    };
+    "git:precommit" = {
+      exec = "pre-commit run --all";
+    };
+  };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
