2037bd2f16807322ac0ff248ff11719bde9adfd6
jarneamerlinck <jarneamerlinck@pm.me>
2026-06-14 09:35:44 +0200

    fix: fixed better naming convention for disk encryption

--- hosts/features/hardware/hibernation.nix
+++ hosts/features/hardware/hibernation.nix
@@ -1,9 +1,12 @@
-{ config, ... }:
+{ config, lib, ... }:
+let
+  disk = config.disko.devices.disk.boot_disk.device;
+in
 {
   services.logind.settings.Login = {
     HandleLidSwitch = if config.powerManagement.enable then "hibernate" else "suspend";
   };
-  boot.resumeDevice = "${config.disko.devices.disk.boot_disk.device}3";
+  boot.resumeDevice = if lib.hasPrefix "/dev/disk/by-" disk then "${disk}-part3" else "${disk}3";
   boot.kernelParams = [ "resume_offset=0" ]; # This is because it's a sepperate partition
   powerManagement.enable = true;
   systemd.sleep.settings.Sleep = {
