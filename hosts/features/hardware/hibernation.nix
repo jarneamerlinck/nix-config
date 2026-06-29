20d20b6cd63fd38ef6c92bec7886990ac0840235
jarneamerlinck <jarneamerlinck@pm.me>
2026-06-29 19:20:35 +0200

    feat: update handle power key

--- hosts/features/hardware/hibernation.nix
+++ hosts/features/hardware/hibernation.nix
@@ -5,6 +5,8 @@ in
 {
   services.logind.settings.Login = {
     HandleLidSwitch = if config.powerManagement.enable then "hibernate" else "suspend";
+    HandlePowerKey = if config.powerManagement.enable then "hibernate" else "suspend";
+    HandlePowerKeyLongPress = "poweroff"; # optional
   };
   boot.resumeDevice = if lib.hasPrefix "/dev/disk/by-" disk then "${disk}-part3" else "${disk}3";
   boot.kernelParams = [ "resume_offset=0" ]; # This is because it's a sepperate partition
